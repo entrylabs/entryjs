a92400b92fdb0bb2a0fdcd8d3d28168b268741c5
SeonMyungLim <brainlip@gmail.com>
2015-08-07 17:45:17 +0900

    entity scale NaN bug fix
    entity width and height 0 makes scale NaN

--- src/stage.js
+++ src/stage.js
@@ -390,11 +390,12 @@ Entry.Stage.prototype.updateHandle = function() {
         entity.setHeight(handle.height / entity.getScaleY());
         entity.setWidth(handle.width / entity.getScaleX());
     } else {
-        if (entity.getScaleX() < 0)
-            entity.setScaleX(-handle.width/entity.width);
-        else
-            entity.setScaleX(handle.width/entity.width);
-        entity.setScaleY(handle.height/entity.height);
+        if (entity.width !== 0)
+            entity.setScaleX(Math.sign(entity.getScaleX()) *
+                        handle.width/entity.width);
+
+        if (entity.height !== 0)
+            entity.setScaleY(handle.height/entity.height);
     }
     var direction = handle.rotation / 180 * Math.PI;
     if (entity.type == "textBox") {
