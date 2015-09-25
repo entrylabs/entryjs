c9e6c880e4b39b99474beb6afec71d71fa6de15e
vivid <brainlip@gmail.com>
2015-09-25 11:31:05 +0900

    arcsin arches xrange checker

--- dist/entry.js
+++ dist/entry.js
@@ -856,8 +856,13 @@ Blockly.Blocks.calc_operation = {init:function() {
   this.setInputsInline(!0);
 }};
 Entry.block.calc_operation = function(a, b) {
-  var c = b.getNumberValue("LEFTHAND", b), d = b.getField("VALUE", b), e = 0;
-  -1 < "sin cos tan asin_radian acos_radian atan_radian".split(" ").indexOf(d) && (c = Entry.toRadian(c), d = d.split("_")[0]);
+  var c = b.getNumberValue("LEFTHAND", b), d = b.getField("VALUE", b);
+  if (-1 < ["asin_radian", "acos_radian"].indexOf(d) && (1 < c || -1 > c)) {
+    throw Error("x range exceeded");
+  }
+  d.indexOf("_") && (d = d.split("_")[0]);
+  -1 < ["sin", "cos", "tan"].indexOf(d) && (c = Entry.toRadian(c));
+  var e = 0;
   switch(d) {
     case "square":
       e = c * c;
@@ -6140,20 +6145,22 @@ Entry.EntryObject.prototype.initEntity = function(a) {
     var c = a.sprite.pictures[0].dimension;
     b.regX = c.width / 2;
     b.regY = c.height / 2;
-    b.scaleX = b.scaleY = "background" == a.sprite.category.main ? Math.max(270 / c.height, 480 / c.width) : "new" == a.sprite.category.main ? 1 : 200 / (c.width + c.height);
+    a = "background" == a.sprite.category.main ? Math.max(270 / c.height, 480 / c.width) : "new" == a.sprite.category.main ? 1 : 200 / (c.width + c.height);
+    b.scaleX = b.scaleY = a;
     b.width = c.width;
     b.height = c.height;
   } else {
     if ("textBox" == this.objectType) {
       if (b.regX = 25, b.regY = 12, b.scaleX = b.scaleY = 1.5, b.width = 50, b.height = 24, b.text = a.name, a.options) {
-        if (a = a.options, c = "", a.bold && (c += "bold "), a.italic && (c += "italic "), b.underline = a.underline, b.strike = a.strike, b.font = c + "20px " + a.font.family, b.colour = a.colour, b.bgColor = a.background, b.lineBreak = a.lineBreak) {
-          a = b.text.split("\n");
-          if (1 < a.length) {
-            for (var c = a[0].length, d = 1, e = a.length;d < e;d++) {
-              a[d].length > c && (c = a[d].length);
+        if (c = a.options, a = "", c.bold && (a += "bold "), c.italic && (a += "italic "), b.underline = c.underline, b.strike = c.strike, b.font = a + "20px " + c.font.family, b.colour = c.colour, b.bgColor = c.background, b.lineBreak = c.lineBreak) {
+          c = b.text.split("\n");
+          if (1 < c.length) {
+            a = c[0].length;
+            for (var d = 1, e = c.length;d < e;d++) {
+              c[d].length > a && (a = c[d].length);
             }
-            b.width = 25 * c;
-            b.height = 24 * a.length;
+            b.width = 25 * a;
+            b.height = 24 * c.length;
           } else {
             b.width = 25 * b.text.length;
           }
@@ -6723,7 +6730,8 @@ Entry.Painter.prototype.colorPixel = function(a, b, c, d, e) {
   this.colorLayerData.data[a + 3] = e;
 };
 Entry.Painter.prototype.pickStrokeColor = function(a) {
-  a = 4 * (Math.round(a.stageY) * this.canvas.width + Math.round(a.stageX));
+  var b = Math.round(a.stageX);
+  a = 4 * (Math.round(a.stageY) * this.canvas.width + b);
   this.stroke.lineColor = Entry.rgb2hex(this.colorLayerData.data[a], this.colorLayerData.data[a + 1], this.colorLayerData.data[a + 2]);
   document.getElementById("entryPainterAttrCircle").style.backgroundColor = this.stroke.lineColor;
   document.getElementById("entryPainterAttrCircleInput").value = this.stroke.lineColor;
@@ -10373,8 +10381,8 @@ Entry.Variable.prototype.setType = function(a) {
   this.type = a;
 };
 Entry.Variable.prototype.getSlidePosition = function(a) {
-  var b = this.minValue_;
-  return Math.abs(this.value_ - b) / Math.abs(this.maxValue_ - b) * a + 10;
+  var b = this.minValue_, c = this.maxValue_, b = Math.abs(this.value_ - b) / Math.abs(c - b);
+  return a * b + 10;
 };
 Entry.Variable.prototype.setSlideCommandX = function(a, b) {
   var c = this.valueSetter_.graphics.command;
@@ -10383,7 +10391,7 @@ Entry.Variable.prototype.setSlideCommandX = function(a, b) {
   this.updateSlideValueByView();
 };
 Entry.Variable.prototype.updateSlideValueByView = function() {
-  var a = Math.max(this.valueSetter_.graphics.command.x - 10, 0) / this.maxWidth;
+  var a = this.maxWidth, a = Math.max(this.valueSetter_.graphics.command.x - 10, 0) / a;
   0 > a && (a = 0);
   1 < a && (a = 1);
   a = (this.minValue_ + Number(Math.abs(this.maxValue_ - this.minValue_) * a)).toFixed(2);
