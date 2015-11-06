da83334252e62061e27872f33d16f40b4d23f3fd
wererewe <wererewe52@gmail.com>
2015-11-06 10:15:01 +0900

    semicolon error(container.js)

--- src/container.js
+++ src/container.js
@@ -69,7 +69,7 @@ Entry.Container.prototype.generateView = function(containerView, option) {
         addButton.innerHTML = Lang.Workspace.add_object;
         addButton.bindOnClick(function(e){
             Entry.dispatchEvent('openSpriteManager');
-        })
+        });
         //this.view_.appendChild(addButton);
 
         var ulWrapper = Entry.createElement('div');
@@ -116,7 +116,7 @@ Entry.Container.prototype.generateView = function(containerView, option) {
         addButton.innerHTML = Lang.Workspace.add_object;
         addButton.bindOnClick(function(e){
             Entry.dispatchEvent('openSpriteManager');
-        })
+        });
         //this.view_.appendChild(addButton);
 
         var ulWrapper = Entry.createElement('div');
@@ -197,7 +197,7 @@ Entry.Container.prototype.setObjects = function(objectModels) {
             Entry.playground.generateSoundElement(s);
         });
     }
-    this.updateObjectsOrder()
+    this.updateObjectsOrder();
     this.updateListView();
     Entry.stage.sortZorder();
     Entry.variableContainer.updateViews();
@@ -335,7 +335,7 @@ Entry.Container.prototype.selectObject = function(objectId, changeScene) {
         if (object.view_)
             object.view_.addClass('selectedObject');
         object.isSelected_ = true;
-    };
+    }
     if (Entry.playground)
         Entry.playground.injectObject(object);
     if (Entry.type != "minimize" && Entry.engine.isState('stop'))
@@ -360,7 +360,7 @@ Entry.Container.prototype.getObject = function(objectId) {
         var object = this.objects_[i];
         if (object.id == objectId)
             return object;
-    };
+    }
 };
 
 /**
@@ -390,7 +390,7 @@ Entry.Container.prototype.getVariable = function(variableId) {
             return variable;
         if (variable.getName() == variableId)
             return variable;
-    };
+    }
 };
 
 /**
@@ -644,7 +644,7 @@ Entry.Container.prototype.getDropdownList = function(menuName) {
             if (variable.object_ && variable.object_ != Entry.playground.object.id)
                 continue;
             result.push([variable.getName(), variable.getId()]);
-        };
+        }
         if (!result || result.length == 0)
             result.push([Lang.Blocks.VARIABLE_variable, 'null']);
     } else if (menuName == 'lists') {
@@ -652,7 +652,7 @@ Entry.Container.prototype.getDropdownList = function(menuName) {
         for (var i = 0; i<lists.length; i++) {
             var list = lists[i];
             result.push([list.getName(), list.getId()]);
-        };
+        }
         if (!result || result.length == 0)
             result.push([Lang.Blocks.VARIABLE_list, 'null']);
     } else if (menuName == 'scenes') {
@@ -660,13 +660,13 @@ Entry.Container.prototype.getDropdownList = function(menuName) {
         for (var i = 0; i<scenes.length; i++) {
             var scene = scenes[i];
             result.push([scene.name, scene.id]);
-        };
+        }
     } else if (menuName == 'sounds') {
         var sounds = Entry.playground.object.sounds;
         for (var i = 0; i<sounds.length; i++) {
             var sound = sounds[i];
             result.push([sound.name, sound.id]);
-        };
+        }
     } else if (menuName == 'clone') {
         result.push([Lang.Blocks.oneself, 'self']);
         var length = this.objects_.length;
@@ -681,7 +681,7 @@ Entry.Container.prototype.getDropdownList = function(menuName) {
         }
     }
     if (!result.length) {
-        result = [[Lang.Blocks.no_target, 'null']]
+        result = [[Lang.Blocks.no_target, 'null']];
     }
     return result;
 };
@@ -695,7 +695,7 @@ Entry.Container.prototype.clearRunningState = function() {
         for (var j = object.clonedEntities.length; j>0; j--) {
             var entity = object.clonedEntities[j-1];
             entity.removeClone();
-        };
+        }
         object.clonedEntities = [];
     });
 };
@@ -713,7 +713,7 @@ Entry.Container.prototype.mapObject = function(mapFunction, param) {
         var object = this.objects_[i];
         mapFunction(object, param);
     }
-}
+};
 
 
 Entry.Container.prototype.mapObjectOnScene = function(mapFunction, param) {
@@ -731,7 +731,7 @@ Entry.Container.prototype.clearRunningStateOnScene = function() {
         for (var j = object.clonedEntities.length; j>0; j--) {
             var entity = object.clonedEntities[j-1];
             entity.removeClone();
-        };
+        }
         object.clonedEntities = [];
     });
 };
@@ -779,7 +779,7 @@ Entry.Container.prototype.mapEntityIncludeClone = function(mapFunction, param) {
             var entity = object.clonedEntities[j];
             if (entity && !entity.isStamp)
                 mapFunction(entity, param);
-        };
+        }
     }
 };
 
@@ -794,7 +794,7 @@ Entry.Container.prototype.mapEntityIncludeCloneOnScene = function(mapFunction, p
             var entity = object.clonedEntities[j];
             if (entity && !entity.isStamp)
                 mapFunction(entity, param);
-        };
+        }
     }
 };
 
@@ -827,7 +827,7 @@ Entry.Container.prototype.toJSON = function() {
     for (var i = 0; i<length; i++) {
         var object = this.objects_[i];
         json.push(object.toJSON());
-    };
+    }
     return json;
 };
 
@@ -875,7 +875,7 @@ Entry.Container.prototype.getVariableJSON = function() {
     for (var i = 0; i<this.variables_.length; i++) {
         var variable = this.variables_[i];
         json.push(variable.toJSON());
-    };
+    }
     return json;
 };
 
@@ -1092,7 +1092,7 @@ Entry.Container.prototype.changeTabView = function(tab) {
 };
 
 Entry.Container.prototype.initYoutube = function(youtubeHash) {
-    this.youtubeHash = youtubeHash
+    this.youtubeHash = youtubeHash;
     this.youtubeTab.removeClass('entryRemove');
     var view = this.view_;
     var width = view.style.width.substring(0,
@@ -1110,7 +1110,7 @@ Entry.Container.prototype.initYoutube = function(youtubeHash) {
 };
 
 Entry.Container.prototype.initTvcast = function(tvcast) {
-    this.tvcast = tvcast
+    this.tvcast = tvcast;
     this.youtubeTab.removeClass('entryRemove');
     var view = this.view_;
     var width = view.style.width.substring(0,
@@ -1140,7 +1140,7 @@ Entry.Container.prototype.initDoneProject = function(projectId) {
     iframe.setAttribute('src', url + this.doneProject);
     this.doneProjectFrame = iframe;
     this.doneContainer.appendChild(iframe);
-}
+};
 
 Entry.Container.prototype.blurAllInputs = function() {
     var objects = this.getSceneObjects();
@@ -1149,7 +1149,7 @@ Entry.Container.prototype.blurAllInputs = function() {
         for (var i=0, len=inputs.length; i<len; i++)
             inputs[i].blur();
     });
-}
+};
 
 Entry.Container.prototype.showProjectAnswer = function() {
     var answer = this.inputValue;
