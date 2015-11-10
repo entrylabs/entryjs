3550f00e0750530d4f270e0deb39fad23287f887
parksejin <nnmm666@naver.com>
2015-11-10 11:22:31 +0900

    generate image

--- src/workspace/workspace.js
+++ src/workspace/workspace.js
@@ -14,7 +14,8 @@ Entry.Workspace = function(blockMenu, board) {
 
     this._blockMenu = blockMenu;
     this._board = board;
-   
+    
+    this.svgGroup = blockMenu.snap.group();
 
     this._playEvent = new Entry.Event(this);
     this._stopEvent = new Entry.Event(this);
@@ -120,6 +121,20 @@ Entry.Workspace = function(blockMenu, board) {
         }, help.duration);
     };
 
+    p.moveMouse = function(startpos, endPos) {
 
+    };
+
+    p.generateImage = function(imagePath) {
+        var block = this.getBoard();
+        var dom = block.svgDom[0];
+        var PosX = dom.clientWidth / 2;
+        var PosY = dom.clientHeight / 2;
+
+        this.svgGroup = this._board.snap.group();
+        this.image = this.svgGroup.image (
+                imagePath , PosX, PosY, 30, 30
+            );
+    };
 
 })(Entry.Workspace.prototype);
