9623329bba6edf3bb4799cd2503e15252862ef32
vivid <brainlip@gmail.com>
2016-03-10 17:54:50 +0900

    remove useless code

--- src/workspace/field/block.js
+++ src/workspace/field/block.js
@@ -160,7 +160,6 @@ Entry.Utils.inherit(Entry.Field, Entry.FieldBlock);
         if (this._posObserver) this._posObserver.destroy();
 
         var view = this._setValueBlock(block).view;
-        //view._toLocalCoordinate();
         view.bindPrev();
         this._blockView.alignContent();
         this._posObserver = view.observe(this, "_updateValueBlock", ["x", "y"], false);
