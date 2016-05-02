319906d8e0df07f19400e8dcbab5da8d31879744
kimorkim <kimorkim@gmail.com>
2016-05-02 12:02:36 +0900

    popup helper conflict

--- src/popup_helper.js
+++ src/popup_helper.js
@@ -9,33 +9,22 @@
  */
 Entry.popupHelper = function(reset) {
     this.popupList = {};
-<<<<<<< HEAD
-    this.nowPopup;
-=======
     this.nowContent;
->>>>>>> master
     if(reset) {
         window.popupHelper = null;
     }
     Entry.assert(!window.popupHelper, 'Popup exist');    
 
-<<<<<<< HEAD
-=======
     var ignoreCloseType = ['confirm', 'spinner'];
->>>>>>> master
     var spanArea = ['entryPopupHelperTopSpan', 'entryPopupHelperBottomSpan', 'entryPopupHelperLeftSpan', 'entryPopupHelperRightSpan'];
     this.body_ = Entry.Dom('div', {
         classes: ['entryPopup', 'hiddenPopup', 'popupHelper'],
     })
-<<<<<<< HEAD
-    this.body_.bindOnClick(function(e) {
-=======
     var that = this;
     this.body_.bindOnClick(function(e) {
         if(that.nowContent && ignoreCloseType.indexOf(that.nowContent.prop('type')) > -1) {
             return;
         }
->>>>>>> master
         var $target = $(e.target);
         spanArea.forEach((function (className) {
             if($target.hasClass(className)) {
@@ -88,44 +77,22 @@ Entry.popupHelper.prototype.clearPopup = function() {
 Entry.popupHelper.prototype.addPopup = function(key, popupObject) {
     var content_ = Entry.Dom('div');
 
-<<<<<<< HEAD
-    var title_ = Entry.Dom('div', {
-        class: 'entryPopupHelperTitle'
-    });
-
-=======
->>>>>>> master
     var titleButton_ = Entry.Dom('div', {
         class: 'entryPopupHelperCloseButton'
     });
 
     titleButton_.bindOnClick((function () {
-<<<<<<< HEAD
-        this.hide();
-=======
         if(popupObject.closeEvent) {
             popupObject.closeEvent(this);   
         } else {
             this.hide();
         }
->>>>>>> master
     }).bind(this));
 
     var popupWrapper_ = Entry.Dom('div', {
         class: 'entryPopupHelperWrapper'
     });
     popupWrapper_.append(titleButton_);
-<<<<<<< HEAD
-    popupWrapper_.append(title_);
-
-    content_.append(popupWrapper_);
-    content_.popupWrapper_ = popupWrapper_;
-
-    title_.text(popupObject.title);
-    if(typeof popupObject.setPopupLayout === 'function') {
-        popupObject.setPopupLayout(content_);
-        content_.prop('popup', popupObject);
-=======
 
     if(popupObject.title) {
         var title_ = Entry.Dom('div', {
@@ -142,7 +109,6 @@ Entry.popupHelper.prototype.addPopup = function(key, popupObject) {
     
     if(typeof popupObject.setPopupLayout === 'function') {
         popupObject.setPopupLayout(content_);
->>>>>>> master
     }
 
     this.popupList[key] = content_;
@@ -152,18 +118,6 @@ Entry.popupHelper.prototype.hasPopup = function(key) {
     return !!this.popupList[key];
 }
 
-<<<<<<< HEAD
-/**
- * Remove this popup
- */
-Entry.popupHelper.prototype.remove = function() {
-    var popupObject = this.nowPopup.prop('popup');
-    if(popupObject && 'onRemove' in popupObject) {
-        popupObject.onRemove();
-    }
-    Entry.removeElement(this.body_);
-    window.popupHelper = null;
-=======
 Entry.popupHelper.prototype.setPopup = function(popupObject) {
 
 };
@@ -179,7 +133,6 @@ Entry.popupHelper.prototype.remove = function(key) {
     delete this.popupList[key];
     this.nowContent = undefined;
     this.body_.addClass('hiddenPopup');
->>>>>>> master
 };
 
 /**
@@ -194,30 +147,13 @@ Entry.popupHelper.prototype.show = function(key) {
     if(this.window_.children().length > 0) {
         this.window_.children().detach();   
     }
-<<<<<<< HEAD
-
-    this.nowPopup = this.popupList[key];
-    this.window_.append(this.nowPopup);
-    var popupObject = this.nowPopup.prop('popup');
-    if(popupObject && 'onShow' in popupObject) {
-        popupObject.onShow();
-    }
-=======
     this.window_.append(this.popupList[key]);
     this.nowContent = this.popupList[key];
->>>>>>> master
     this.body_.removeClass('hiddenPopup');
 };
 
 
 Entry.popupHelper.prototype.hide = function() {
-<<<<<<< HEAD
-    var popupObject = this.nowPopup.prop('popup');
-    if(popupObject && 'onHide' in popupObject) {
-        popupObject.onHide();
-    }
-=======
     this.nowContent = undefined;
->>>>>>> master
     this.body_.addClass('hiddenPopup');
 };
