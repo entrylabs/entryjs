459ece2bfa3da55150934a4e1568d7a3cf6f4cf3
jhlee <geniusjuno@gmail.com>
2016-05-17 21:32:03 +0900

    commit

---
+++ src/textcoding/exception/particularBlockProcessing.js
@@ -0,0 +1,30 @@
+/*
+ *
+ */
+"use strict";
+
+goog.provide("Entry.ParticularBlockProcessing");
+
+Entry.ParticularBlockProcessing = function() {
+    
+};
+
+(function(p){
+    p.isParticularBlock = function(blockType) {
+    	console.log("isParticularBlock", blockType);
+    	var result = false;
+    	switch(blockType) {
+            case 'reach_something': {
+                result = true;
+                break;
+            }
+            default : {
+                result = false;
+                break;
+            }
+        }
+        
+        return result;
+    };
+
+})(Entry.ParticularBlockProcessing.prototype);
