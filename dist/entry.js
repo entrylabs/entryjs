c09a956768fff61663aa3132c256805a4e209e00
jykim <kimorkim@gmail.com>
2016-05-22 12:07:35 +0900

    change download path

--- dist/entry.js
+++ dist/entry.js
@@ -12976,7 +12976,7 @@ p.closeConnection = function() {
   this.socket && this.socket.close();
 };
 p.downloadConnector = function() {
-  window.open("http://download.play-entry.org/apps/Entry_HW_1.5.2_Setup.exe", "_blank").focus();
+  window.open("http://download.play-entry.org/apps/Entry_HW_1.5.2.exe", "_blank").focus();
 };
 p.downloadSource = function() {
   window.open("http://play-entry.com/down/board.ino", "_blank").focus();
