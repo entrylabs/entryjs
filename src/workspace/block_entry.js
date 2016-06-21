772fe8e1c90af11204982d8a51386dd984f2c3e2
jykim <kimorkim@gmail.com>
2016-06-21 17:35:21 +0900

    add kocoa fab joystick hw

--- src/workspace/block_entry.js
+++ src/workspace/block_entry.js
@@ -18998,6 +18998,109 @@ Entry.block = {
         },
         "class": "arduino"
     },
+    "joystick_get_number_sensor_value": {
+        "parent": "arduino_get_number_sensor_value",
+        "isNotFor": [
+            "joystick"
+        ],
+        "def": {
+            "params": [
+                {
+                    "type": "arduino_get_sensor_number"
+                }
+            ],
+            "type": "joystick_get_number_sensor_value"
+        },
+        "class": "arduino_value"
+    },
+    "joystick_get_digital_value": {
+        "parent": "arduino_get_digital_value",
+        "isNotFor": [
+            "joystick"
+        ],
+        "def": {
+            "params": [
+                {
+                    "type": "arduino_get_port_number"
+                }
+            ],
+            "type": "joystick_get_digital_value"
+        },
+        "class": "arduino_value"
+    },
+    "joystick_toggle_led": {
+        "parent": "arduino_toggle_led",
+        "isNotFor": [
+            "joystick"
+        ],
+        "def": {
+            "params": [
+                {
+                    "type": "arduino_get_port_number"
+                },
+                null,
+                null
+            ],
+            "type": "joystick_toggle_led"
+        },
+        "class": "arduino_set"
+    },
+    "joystick_toggle_pwm": {
+        "parent": "arduino_toggle_pwm",
+        "isNotFor": [
+            "joystick"
+        ],
+        "def": {
+            "params": [
+                {
+                    "type": "arduino_get_pwm_port_number"
+                },
+                {
+                    "type": "arduino_text",
+                    "params": [ "255" ]
+                },
+                null
+            ],
+            "type": "joystick_toggle_pwm"
+        },
+        "class": "arduino_set"
+    },
+    "joystick_convert_scale": {
+        "parent": "arduino_convert_scale",
+        "isNotFor": [
+            "joystick"
+        ],
+        "def": {
+            "params": [
+                {
+                    "type": "arduino_get_number_sensor_value",
+                    "params": [
+                        {
+                            "type": "arduino_get_sensor_number"
+                        }
+                    ]
+                },
+                {
+                    "type": "number",
+                    "params": [ "0" ]
+                },
+                {
+                    "type": "number",
+                    "params": [ "1023" ]
+                },
+                {
+                    "type": "number",
+                    "params": [ "0" ]
+                },
+                {
+                    "type": "number",
+                    "params": [ "100" ]
+                }
+            ],
+            "type": "joystick_convert_scale"
+        },
+        "class": "arduino"
+    },
     "ebs_if": {
         "parent": "_if",
         "def": {
