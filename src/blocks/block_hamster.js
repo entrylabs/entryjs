8b702a5c9ba05e22015e82f4d9bb901723172b88
chanlee <wonchan.lee@gmail.com>
2016-08-17 15:41:30 +0900

    change albert and hamster port monitoring

--- src/blocks/block_hamster.js
+++ src/blocks/block_hamster.js
@@ -53,7 +53,36 @@ Entry.Hamster = {
 		sq.lineTracerMode = mode;
 		sq.lineTracerModeId = this.lineTracerModeId;
 	},
-	name: 'hamster'
+	name: 'hamster',
+    monitorTemplate: {
+        imgPath: "hw/hamster.png",
+        width: 256,
+        height: 256,
+        listPorts:{
+            "temperature":{name: Lang.Blocks.HAMSTER_sensor_temperature, type: "input", pos: {x: 0, y: 0}},
+            "inputA":{name: Lang.Blocks.HAMSTER_sensor_input_a, type: "input", pos: {x: 0, y: 0}},
+            "inputB":{name: Lang.Blocks.HAMSTER_sensor_input_b, type: "input", pos: {x: 0, y: 0}},
+            "accelerationX":{name: Lang.Blocks.HAMSTER_sensor_acceleration_x, type: "input", pos: {x: 0, y: 0}},
+            "accelerationY":{name: Lang.Blocks.HAMSTER_sensor_acceleration_y, type: "input", pos: {x: 0, y: 0}},
+            "accelerationZ":{name: Lang.Blocks.HAMSTER_sensor_acceleration_z, type: "input", pos: {x: 0, y: 0}},
+            "buzzer":{name: Lang.Hw.buzzer , type: "output", pos: {x: 0, y: 0}},
+            "note":{name:  Lang.Hw.note , type: "output", pos: {x: 0, y: 0}},
+            "outputA":{name: Lang.Hw.output + "A", type: "output", pos: {x: 0, y: 0}},
+            "outputB":{name: Lang.Hw.output + "B", type: "output", pos: {x: 0, y: 0}}
+        },
+        ports: {
+            "leftProximity":{name: Lang.Blocks.HAMSTER_sensor_left_proximity, type: "input", pos: {x: 122, y: 156}},
+            "rightProximity":{name: Lang.Blocks.HAMSTER_sensor_right_proximity, type: "input", pos: {x : 10, y: 108}},
+            "leftFloor":{name: Lang.Blocks.HAMSTER_sensor_left_floor, type: "input", pos: {x: 100, y: 234}},
+            "rightFloor":{name: Lang.Blocks.HAMSTER_sensor_right_floor, type: "input", pos: {x: 13, y: 180}},
+            "light":{name: Lang.Blocks.HAMSTER_sensor_light, type: "input", pos: {x: 56, y: 189}},
+            "leftWheel":{name: Lang.Hw.leftWheel, type: "output", pos: {x: 209, y: 115}},
+            "rightWheel":{name: Lang.Hw.rightWheel, type: "output", pos: {x: 98, y: 30}},
+            "leftLed":{name: Lang.Hw.left + " " + Lang.Hw.led_en, type: "output", pos: {x: 87, y: 210}},
+            "rightLed":{name: Lang.Hw.right + " " + Lang.Hw.led_en , type: "output", pos: {x: 24, y: 168}},
+        },
+      mode : 'both'
+    }
 };
 
 //sensor
