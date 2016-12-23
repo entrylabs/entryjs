2d0ead4e6b1417d11f40b570bdd4b3fc8bb1d4ed
vivid <brainlip@gmail.com>
2016-12-23 17:21:18 +0900

    remove console

--- src/property_panel.js
+++ src/property_panel.js
@@ -44,8 +44,6 @@ Entry.PropertyPanel = function() {
     };
 
     p.addMode = function(mode, contentObj) {
-        console.log("addMode", mode);
-
         var contentDom = contentObj.getView();
         // will be removed after apply new Dom class
         contentDom = Entry.Dom(contentDom, {
