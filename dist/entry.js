33d3714c82136bc0aa2b57fa8186e8b780bc5770
vivid <brainlip@gmail.com>
2017-04-07 16:22:45 +0900

    do not destroy content

--- dist/entry.js
+++ dist/entry.js
@@ -22133,9 +22133,6 @@ Entry.BlockView.RENDER_MODE_TEXT = 2;
     b = void 0 === b ? this.renderMode : b;
     this.contentSvgGroup && this.contentSvgGroup.remove();
     this.statementSvgGroup && this.statementSvgGroup.remove();
-    this._contents.forEach(function(b) {
-      b.destroy();
-    });
     this.contentSvgGroup = this.svgGroup.elem("g", {class:"contentsGroup"});
     this._contents = [];
     var c = this._schema;
