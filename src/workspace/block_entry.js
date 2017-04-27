136ffb4685610c43791da9570c1fc2c24abf694b
Kyumin Sim <kyumin92@gmail.com>
2017-04-27 23:27:06 +0900

    asdf

--- src/workspace/block_entry.js
+++ src/workspace/block_entry.js
@@ -34875,7 +34875,7 @@ Entry.block = {
             }
             if (returnVal)
                 return;
-            else if (flow === 0)
+            else if (flow == 0)
                 return Entry.STATIC.BREAK;
             else
                 this.die();
