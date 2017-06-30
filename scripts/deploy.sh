47b9d6c53f4df35b3fef890784085e1ed52c7963
JY KIM <kimorkim@gmail.com>
2017-06-30 17:35:23 +0900

    r

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e # 에러 발생 시 스크립트 중단
 
+rm .gitignore
+
 DATEFMT=`date "+%Y%m%d%H%M%S"`
 # echo "$DATEFMT: My message"
 
@@ -9,7 +11,7 @@ git config user.email "kimorkim@gmail.com"
 git add dist/*
 git commit -m "Entry Js Update"
 git tag -fa "$DATEFMT"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" build
 
 
 
