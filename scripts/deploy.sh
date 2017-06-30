9e79ff99585d47b9e4133fa693a1d49af04bc722
JY KIM <kimorkim@gmail.com>
2017-06-30 17:10:47 +0900

    add script

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e # 에러 발생 시 스크립트 중단
+
+DATEFMT=`date "+%Y%m%d%H%M%S"`
+# echo "$DATEFMT: My message"
+
+git config user.name "JY KIM"
+git config user.email "kimorkim@gmail.com"
+git add dist
+git commit -m "Entry Js Update"
+git tag -fa "$DATEFMT"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}"
+
+
+
