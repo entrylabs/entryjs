b7b458ffa17118b19d2c7f87c800396d30fd38b4
jykim <kimorkim@gmail.com>
2017-07-03 15:27:42 +0900

    update deploy on master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,19 +5,11 @@ rm .gitignore
 
 DATEFMT=`date "+%Y%m%d"`
 # echo "$DATEFMT: My message"
-echo '1'
 cd build
-echo '2'
 git config user.name "JY KIM"
-echo '3'
 git config user.email "kimorkim@gmail.com"
-echo '4'
 git add .
-echo '5'
 git commit -m "Entry Js PUBLISH by v3.$DATEFMT.${TRAVIS_BUILD_NUMBER}"
-echo '6'
 git tag -a "v3.$DATEFMT.${TRAVIS_BUILD_NUMBER}" -m "build v3.$DATEFMT.${TRAVIS_BUILD_NUMBER}"
-echo '7'
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" build --tags
-echo '8'
 #curl -d '{"tag_name": "v$DATEFMT","target_commitish": "build","name": "v$DATEFMT","body": "Description of the release","draft": false,"prerelease": false}' -X POST "https://developer.github.com/v3/repos/kimokim/entryjs/releases"
