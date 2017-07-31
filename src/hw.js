0e2b8e0ef22ea738ce52f8ab13e563146326d452
jykim <kimorkim@gmail.com>
2017-07-31 15:40:46 +0900

    update hardware version update

--- src/hw.js
+++ src/hw.js
@@ -16,8 +16,8 @@ Entry.HW = function() {
     this.isFirstConnect = true;
     //this.requireVerion = 'v1.6.1';
     this.requireVerion = 'v1.6.1';
-    this.downloadPath = "http://download.play-entry.org/apps/Entry_HW_1.6.11_Setup.exe";
-    this.downloadPathOsx = "http://download.play-entry.org/apps/Entry_HW-1.6.11.dmg";
+    this.downloadPath = "http://download.play-entry.org/apps/Entry_HW_1.6.12_Setup.exe";
+    this.downloadPathOsx = "http://download.play-entry.org/apps/Entry_HW-1.6.12.dmg";
     this.hwPopupCreate();
     this.initSocket();
     this.connected = false;
