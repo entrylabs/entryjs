e450044ac0ab435c176eb4601d97951193fc6dc3
Kyumin Sim <kyumin92@gmail.com>
2017-08-30 16:05:52 +0900

    add value

--- src/textcoding/parser/core/text/pyToBlock.js
+++ src/textcoding/parser/core/text/pyToBlock.js
@@ -149,7 +149,7 @@ Entry.PyToBlockParser = function(blockSyntax) {
         }
 
         if(obj.type == 'is_press_some_key') {
-            obj.params = [ Entry.KeyboardCode.map[ component.arguments[0].value ]];
+            obj.params = [ Entry.KeyboardCode.map[ component.arguments[0].value ] + ""];
         }
 
 
