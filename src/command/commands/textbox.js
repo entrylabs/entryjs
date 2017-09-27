66680515868ecced6ae549cc7664c7192839dc31
kimorkim <kimorkim@gmail.com>
2017-09-27 13:49:57 +0900

    textbox editText command 추가

---
+++ src/command/commands/textbox.js
@@ -0,0 +1,30 @@
+/*
+ *
+ */
+"use strict";
+
+goog.require("Entry.Command");
+goog.require("Entry.STATIC");
+
+(function(c) {
+    var COMMAND_TYPES = Entry.STATIC.COMMAND_TYPES;
+
+    c[COMMAND_TYPES.editText] = {
+        do: function(text, prevText) {
+            Entry.playground.object.setText(text);
+            Entry.playground.object.entity.setText(text);
+            Entry.dispatchEvent('textEdited');
+        },
+        state: function(text, prevText) {
+            return [prevText, text];
+        },
+        log: function() {
+            return [];
+        },
+        validate: false,
+        recordable: Entry.STATIC.RECORDABLE.SUPPORT,
+        undo: "editText",
+    };
+
+})(Entry.Command);
+
