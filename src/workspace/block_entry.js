352de63e4bae0397cf82891ba68aacb2896b6903
jykim <kimorkim@gmail.com>
2017-11-29 17:01:51 +0900

    update block error

--- src/workspace/block_entry.js
+++ src/workspace/block_entry.js
@@ -32246,7 +32246,7 @@ Entry.block = {
     "neobot_equal_with_sensor": {//MinjuneL
         "color": "#00979D",
         "skeleton": "basic_boolean_field",
-        "fontColor": "#fff"
+        "fontColor": "#fff",
         "template" : "%1 의 센서값이 %2",
         "statements": [],
         "params": [
