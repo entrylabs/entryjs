479d3b607c46d98c974497a9dae0bcb690c156e2
jykim <kimorkim@gmail.com>
2018-07-18 17:06:49 +0900

    update build & deploy script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,15 @@
 set -e # 에러 발생 시 스크립트 중단
 
 export NODE_ENV=production
+rm -rf build || exit 0
 git clone -b build "${GH_REPO}" build
 rm -rf build/**/* || exit 0
 npm run dist
 cp -r dist build/
 cp -r extern build/
 cp -r images build/
-rsync -R src/playground/block_entry.js build
-rsync -R src/playground/block_entry_mini.js build
-rsync -r -R src/playground/blocks/ build
+mkdir -p build/src/playground/blocks
+cp -r src/playground/blocks/ build/src/playground/
+cp src/playground/block_entry.js src/playground/block_entry_mini.js build/src/playground/
 ls -al
 ls -al build
