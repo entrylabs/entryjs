ae04c4799d0749dc4a33322f835866d71ff983f6
junbae.park <junbae.park@nts-corp.com>
2018-08-08 10:16:49 +0900

    [variable.js] var 타입 기본 커서 move 로 변경

--- src/class/variable.js
+++ src/class/variable.js
@@ -143,7 +143,7 @@ Entry.Variable.prototype.generateView = function(variableIndex) {
         }
         this.view_.visible = this.visible_;
         this.view_.addChild(this.valueView_);
-
+        this.view_.cursor = "move";
         this.view_.on(PIXIDragHelper.DOWN, function(evt) {
             if (Entry.type != 'workspace') return;
             PIXIDragHelper.handleDrag(this);
@@ -153,7 +153,6 @@ Entry.Variable.prototype.generateView = function(variableIndex) {
                 x: this.x - (gp.x * 0.75 - 240),
                 y: this.y - (gp.y * 0.75 - 135),
             };
-            this.cursor = 'move';
         });
 
         this.view_.on(PIXIDragHelper.MOVE, function(evt) {
