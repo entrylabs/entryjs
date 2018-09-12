86f825587ee2f4179b927a855be4bc3d7056dd11
NAVER <lee.hungyu@nts-corp.com>
2018-09-12 18:09:24 +0900

    [#9108] 지역코드 시군구 코드 추가.

--- src/playground/blocks/block_expansion_festival.js
+++ src/playground/blocks/block_expansion_festival.js
@@ -21,23 +21,75 @@ Entry.EXPANSION_BLOCK.festival = {
     },
     api: '/api/expansionBlock/ko/festival',
     locationMap: {
-        'Seoul': 1,
-        'Incheon': 2,
-        'Daejeon': 3,
-        'Daegu': 4,
-        'Gwangju': 5,
-        'Busan': 6,
-        'Ulsan': 7,
-        'Sejong': 8,
-        'Gyeonggi-do': 31,
-        'Gangwon': 32,
-        'Chungcheongbuk-do': 33,
-        'Chungcheongnam-do': 34,
-        'Gyeongsangbuk-do': 35,
-        'Gyeongsangnam-do': 36,
-        'Jeollabuk-do': 37,
-        'Jeollanam-do': 38,
-        'Jeju': 39,
+        'Seoul': {
+            code : 1,
+            sub : ["강남구", "강동구", "강북구", "강서구", "관악구", "광진구", "구로구", "금천구", "노원구", "도봉구", "동대문구", "동작구", "마포구", "서대문구", "서초구", "성동구", "성북구", "송파구", "양천구", "영등포구", "용산구", "은평구", "종로구", "중구", "중랑구"]
+        },
+        'Incheon': {
+            code : 2,
+            sub : ["강화군", "계양구", "남구", "남동구", "동구", "부평구", "서구", "연수구", "옹진군", "중구"]
+        },
+        'Daejeon': {
+            code : 3,
+            sub : ["대덕구", "동구", "서구", "유성구", "중구"]
+        },
+        'Daegu': {
+            code : 4,
+            sub : ["남구", "달서구", "달성군", "동구", "북구", "서구", "수성구", "중구"]
+        },
+        'Gwangju': {
+            code :5,
+            sub : ["광산구", "남구", "동구", "북구", "서구"]
+        },
+
+        'Busan': {
+            code :6,
+            sub : ["강서구", "금정구", "기장군", "남구", "동구", "동래구", "부산진구", "북구", "사상구", "사하구", "서구", "수영구", "연제구", "영도구", "중구", "해운대구"]
+        },
+        'Ulsan': {
+            code :7,
+            sub : ["중구", "남구", "동구", "북구", "울주군"]
+        },
+        'Sejong': {
+            code :8,
+            sub : ["세종특별자치시"]
+        },
+        'Gyeonggi-do': {
+            code :31,
+            sub : ["가평군", "고양시", "과천시", "광명시", "광주시", "구리시", "군포시", "김포시", "남양주시", "동두천시", "부천시", "성남시", "수원시", "시흥시", "안산시", "안성시", "안양시", "양주시", "양평군", "여주시", "연천군", "오산시", "용인시", "의왕시", "의정부시", "이천시", "파주시", "평택시", "포천시", "하남시", "화성시"]
+        },
+        'Gangwon': {
+            code :32,
+            sub : ["강릉시", "고성군", "동해시", "삼척시", "속초시", "양구군", "양양군", "영월군", "원주시", "인제군", "정선군", "철원군", "춘천시", "태백시", "평창군", "홍천군", "화천군", "횡성군"]
+        } ,
+        'Chungcheongbuk-do': {
+            code :33,
+            sub : ["괴산군", "단양군", "보은군", "영동군", "옥천군", "음성군", "제천시", "진천군", "청원군", "청주시", "충주시",  "증평군"]
+        },
+        'Chungcheongnam-do': {
+            code: 34,
+            sub : ["공주시", "금산군", "논산시", "당진시", "보령시", "부여군", "서산시", "서천군", "아산시", "예산군", "천안시", "청양군", "태안군", "홍성군", "계룡시"]
+        },
+        'Gyeongsangbuk-do': {
+            code: 35,
+            sub : ["경산시", "경주시", "고령군", "구미시", "군위군", "김천시", "문경시", "봉화군", "상주시", "성주군", "안동시", "영덕군", "영양군", "영주시", "영천시", "예천군", "울릉군", "울진군", "의성군", "청도군", "청송군", "칠곡군", "포항시"]
+        },
+        'Gyeongsangnam-do': {
+            code: 36,
+            sub : ["거제시", "거창군", "고성군", "김해시", "남해군", "마산시", "밀양시", "사천시", "산청군", "양산시", "의령군", "진주시", "진해시", "창녕군", "창원시", "통영시", "하동군", "함안군", "함양군", "합천군"]
+        },
+        'Jeollabuk-do': {
+            code: 37,
+            sub : ["고창군", "군산시", "김제시", "남원시", "무주군", "부안군", "순창군", "완주군", "익산시", "임실군", "장수군", "전주시", "정읍시", "진안군"]
+        },
+        'Jeollanam-do': {
+            code: 38,
+            sub : ["강진군", "고흥군", "곡성군", "광양시", "구례군", "나주시", "담양군", "목포시", "무안군", "보성군", "순천시", "신안군", "여수시", "영광군", "영암군", "완도군", "장성군", "장흥군", "진도군", "함평군", "해남군", "화순군"]
+        },
+        'Jeju': {
+            code: 39,
+            sub : ["남제주군", "북제주군", "서귀포시", "제주시"]
+        },
     },
     infoTypeMap: {
         'title': 'title',
@@ -46,7 +98,7 @@ Entry.EXPANSION_BLOCK.festival = {
         'end_date': 'eventenddate',
         'coordinatex': 'mapx',
         'coordinatey': 'mapy',
-        'area': 'addr2',
+        'area': 'sigungucode',
         'homepage': 'homepage',
         'overview': 'overview',
     },
@@ -182,7 +234,12 @@ Entry.EXPANSION_BLOCK.festival.getBlocks = function() {
         return new PromiseManager().Promise(function(resolve) {
             callApi(key, { url: Entry.EXPANSION_BLOCK.festival.api, params: params }).then((result) => {
                 let items = result.data.response.body.items.item;
-                let item = items[num - 1];
+                let item = null;
+                if(items.constructor == Array) {
+                    item = items[num - 1];
+                }else {
+                    item = items;
+                }
                 return resolve(item);
             }).catch(() => {
                 return resolve(defaultValue);
@@ -238,7 +295,7 @@ Entry.EXPANSION_BLOCK.festival.getBlocks = function() {
             func: async function(sprite, script) {
                 const defaultValue = 0;
                 const params = {
-                    area: Entry.EXPANSION_BLOCK.festival.locationMap[script.getField('LOCATION', script)],
+                    area: Entry.EXPANSION_BLOCK.festival.locationMap[script.getField('LOCATION', script)].code,
                     month: script.getField('MONTH', script),
                     list: 'N',
                 };
@@ -297,14 +354,17 @@ Entry.EXPANSION_BLOCK.festival.getBlocks = function() {
                 const number = await script.getStringValue('NUMBER', script);
                 const type = script.getField('TYPE', script);
                 const infoType = Entry.EXPANSION_BLOCK.festival.infoTypeMap[type];
+                const location = Entry.EXPANSION_BLOCK.festival.locationMap[script.getField('LOCATION', script)];
                 const defaultValue = '정보없음';
                 const params = {
-                    area: Entry.EXPANSION_BLOCK.festival.locationMap[script.getField('LOCATION', script)],
+                    area: location.code,
                     month: script.getField('MONTH', script),
                 };
 
                 const festival = await getFestivals(number, params, {});
                 switch (type) {
+                    case 'area':
+                        return location.sub[festival[infoType] - 1] || defaultValue;
                     case 'homepage':
                     case 'overview':
                         const detailInfo = await getDetailInfo(festival.contentid, {});
