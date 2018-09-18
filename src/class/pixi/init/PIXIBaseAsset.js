239e88836aecdce2bba68dacf472ce132aa546e1
junbae.park <junbae.park@nts-corp.com>
2018-09-18 19:13:20 +0900

    [texture] BaseAsset 약간 수정

--- src/class/pixi/init/PIXIBaseAsset.js
+++ src/class/pixi/init/PIXIBaseAsset.js
@@ -7,12 +7,15 @@ var atlasJson = require("./../../../../entry_texture/base_asset");
 export class PIXIBaseAsset {
 
     constructor() {
-        this._baseTexure = PIXI.BaseTexture.fromImage(Entry.mediaFilePath + "base_asset.png");
-        this._sheet = new PIXI.Spritesheet(this._baseTexure, atlasJson);
+        var path =  Entry.mediaFilePath + "base_asset.png";
+        this._sheet = new PIXI.Spritesheet(
+            PIXI.BaseTexture.fromImage(path),
+            atlasJson
+        );
+
         //서브텍스쳐의 개수는 반드시 1000개보다 작아야 한다. 그렇지 않으면 parse 가 async 로 작동함.
         //1000 이라는 숫자는 PIXI.Spritesheet.BATCH_SIZE getter 에 정의됨.
         this._sheet.parse(()=>{});
-        // this._loadImage();
     }
 
     newSprite(key) {
