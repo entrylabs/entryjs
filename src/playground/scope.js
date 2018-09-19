46e23ca684fe5ebf61a1bc1784b50e48cfe56f7b
extracold1209 <extracold1209@naver.com>
2018-09-19 18:47:48 +0900

    [async] 주석 추가 및 leaf 트리 구현시 닿지않는 분기문 전부 삭제

--- src/playground/scope.js
+++ src/playground/scope.js
@@ -46,6 +46,15 @@ Entry.Scope = function(block, executor) {
         return this._getValueFromValueMap(fieldBlock);
     };
 
+    /**
+     * getValue(s) 에서 사용되는 함수. executor 의 valueMap 에 데이터가 있는지 가져온다.
+     * 데이터가 아예 없는 경우 (말단 값블럭이 아닌경우) func 를 호출한다.
+     * 또한 isResolved === false 인 경우는 Promise 가 pending 중인 경우이므로 AsyncError 를 던진다.
+     *
+     * @param block 값을 가져올 블럭
+     * @returns {*} 블록의 결과 값
+     * @throws Entry.Utils.AsyncError Promise pending 중인 값을 호출한 경우
+     */
     p._getValueFromValueMap = function(block) {
         const result = this.executor.valueMap[block.data.id];
         if (result === undefined) {
@@ -58,10 +67,17 @@ Entry.Scope = function(block, executor) {
         }
     };
 
+    /**
+     * 현재 블록을 기준으로 중첩된 블록을 전부 순회하며 말단 블록을 가져온다.
+     * 말단 블록 리스트는 executor 에 기록되며 한번 기록이 된 이후에는 다음 scope 가 되기전까지 변하지 않는다.
+     * 해당 scope 에서 한번 실행된 함수는 더이상 트리를 순회하지 않는다.
+     * @param rootBlocks 기준 블록
+     */
     p._makeLeafValueTree = function(...rootBlocks) {
         const executorValueMap = this.executor.valueMap;
         const leafBlocks = [];
 
+        // 현재 스코프에서 한번 생성된 트리를 다시 순회하지 않는다.
         if (Object.keys(executorValueMap).length > 0) {
             return;
         }
@@ -84,28 +100,24 @@ Entry.Scope = function(block, executor) {
         leafBlocks.forEach((block) => {
             const blockId = block.data.id;
 
-            if (executorValueMap[blockId] === 'isPending') {
-                throw new Entry.Utils.AsyncError();
-            } else if (executorValueMap[blockId] === undefined) {
-                const newScope = new Entry.Scope(block, this.executor);
-                const result = Entry.block[block.type].func.call(newScope, this.entity, newScope);
+            const newScope = new Entry.Scope(block, this.executor);
+            const result = Entry.block[block.type].func.call(newScope, this.entity, newScope);
 
-                if (result instanceof Promise) {
+            if (result instanceof Promise) {
+                executorValueMap[blockId] = {
+                    isResolved: false,
+                };
+                result.then((value) => {
                     executorValueMap[blockId] = {
-                        isResolved : false,
+                        value,
+                        isResolved: true,
                     };
-                    result.then((value) => {
-                        executorValueMap[blockId] = {
-                            value,
-                            isResolved : true,
-                        };
-                    });
-                } else {
-                    executorValueMap[blockId] = {
-                        value : result,
-                        isResolved : true,
-                    };
-                }
+                });
+            } else {
+                executorValueMap[blockId] = {
+                    value: result,
+                    isResolved: true,
+                };
             }
         });
     };
