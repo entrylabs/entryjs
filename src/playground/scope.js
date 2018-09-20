82c9895baad309ab202d0ff060fa1acfcf05c008
youngduk.park <youngduk.park@nhn.com>
2018-09-20 14:03:17 +0900

    class 문법 적용

--- src/playground/scope.js
+++ src/playground/scope.js
@@ -1,27 +1,27 @@
 'use strict';
 
-Entry.Scope = function(block, executor) {
-    this.block = block;
-    this.type = block ? block.type : null; //legacy
-    this.executor = executor;
-    this.entity = executor.entity;
-};
+class Scope {
+    constructor(block, executor) {
+        this.block = block;
+        this.type = block ? block.type : null; //legacy
+        this.executor = executor;
+        this.entity = executor.entity;
+    }
 
-(function(p) {
-    p.callReturn = function() {
+    callReturn() {
         return undefined;
-    };
+    }
 
-    p.getParam = function(index) {
+    getParam(index) {
         const fieldBlock = this.block.params[index];
         const newScope = new Entry.Scope(fieldBlock, this.executor);
         const result = Entry.block[fieldBlock.type].func.call(newScope, this.entity, newScope);
         return result;
-    };
+    }
 
-    p.getParams = function() {
+    getParams() {
         const that = this;
-        return this.block.params.map(function(param) {
+        return this.block.params.map((param) => {
             if (param instanceof Entry.Block) {
                 const fieldBlock = param;
                 const newScope = new Entry.Scope(fieldBlock, that.executor);
@@ -30,21 +30,25 @@ Entry.Scope = function(block, executor) {
                 return param;
             }
         });
-    };
+    }
 
-    p.getValues = function(keys, scope) {
-        const fieldBlocks = keys.map((key) => this.block.params[this._getParamIndex(key, scope)]);
-        
+    getValues(keys, scope) {
+        const fieldBlocks = keys.map((key) => {
+            return this.block.params[this._getParamIndex(key, scope)];
+        });
+
         this._setValueMap(fieldBlocks);
-        return fieldBlocks.map((fieldBlocks) => this._getValueFromValueMap(fieldBlocks));
-    };
+        return fieldBlocks.map((fieldBlocks) => {
+            return this._getValueFromValueMap(fieldBlocks);
+        });
+    }
 
-    p.getValue = function(key, scope) {
+    getValue(key, scope) {
         const fieldBlock = this.block.params[this._getParamIndex(key, scope)];
 
         this._setValueMap(fieldBlock);
         return this._getValueFromValueMap(fieldBlock);
-    };
+    }
 
     /**
      * getValue(s) 에서 사용되는 함수. executor 의 valueMap 에 데이터가 있는지 가져온다.
@@ -55,7 +59,7 @@ Entry.Scope = function(block, executor) {
      * @returns {*} 블록의 결과 값
      * @throws Entry.Utils.AsyncError Promise pending 중인 값을 호출한 경우
      */
-    p._getValueFromValueMap = function(block) {
+    _getValueFromValueMap(block) {
         const result = this.executor.valueMap[block.data.id];
         if (result === undefined) {
             const newScope = new Entry.Scope(block, this.executor);
@@ -65,7 +69,7 @@ Entry.Scope = function(block, executor) {
         } else {
             throw new Entry.Utils.AsyncError();
         }
-    };
+    }
 
     /**
      * 현재 블록을 기준으로 중첩된 블록을 전부 순회하며 말단 블록을 가져온다.
@@ -73,7 +77,7 @@ Entry.Scope = function(block, executor) {
      * 해당 scope 에서 한번 실행된 함수는 더이상 트리를 순회하지 않는다.
      * @param rootBlocks 기준 블록
      */
-    p._setValueMap = function(blocks) {
+    _setValueMap(blocks) {
         const rootBlocks = blocks instanceof Array ? blocks : [blocks];
         const executorValueMap = this.executor.valueMap;
         // 현재 스코프에서 한번 생성된 트리를 다시 순회하지 않는다.
@@ -102,13 +106,15 @@ Entry.Scope = function(block, executor) {
                 };
             }
         });
-    };
+    }
 
-    p._getLeafBlocks = function(rootBlocks) {
+    _getLeafBlocks(rootBlocks) {
         const leafBlocks = [];
         const addValueBlockRecursive = (block) => {
             const params = block.data && block.data.params;
-            const blockParams = params.filter(value => value instanceof Entry.Block);
+            const blockParams = params.filter((value) => {
+                return value instanceof Entry.Block;
+            });
 
             if (blockParams.length >= 1) {
                 blockParams.forEach((value) => {
@@ -121,57 +127,59 @@ Entry.Scope = function(block, executor) {
 
         rootBlocks.forEach(addValueBlockRecursive);
         return leafBlocks;
-    };
+    }
 
-    p.getStringValue = function(key, scope) {
+    getStringValue(key, scope) {
         return String(this.getValue(key, scope));
-    };
+    }
 
-    p.getNumberValue = function(key, scope) {
+    getNumberValue(key, scope) {
         return Number(this.getValue(key, scope));
-    };
+    }
 
-    p.getBooleanValue = function(key, scope) {
+    getBooleanValue(key, scope) {
         let value = this.getValue(key, scope);
         if (value === undefined) {
             return false;
         }
         value = Number(value);
         return isNaN(value) ? true : value; // handle "0" or "0.00"
-    };
+    }
 
-    p.getField = function(key) {
+    getField(key) {
         return this.block.params[this._getParamIndex(key)];
-    };
+    }
 
-    p.getStringField = function(key) {
+    getStringField(key) {
         return String(this.getField(key));
-    };
+    }
 
-    p.getNumberField = function(key) {
+    getNumberField(key) {
         return Number(this.getField(key));
-    };
+    }
 
-    p.getStatement = function(key, scope) {
+    getStatement(key, scope) {
         return this.executor.stepInto(this.block.statements[this._getStatementIndex(key, scope)]);
-    };
+    }
 
-    p._getParamIndex = function(key) {
+    _getParamIndex(key) {
         if (!this._schema) {
             this._schema = Entry.block[this.type];
         }
         return this._schema.paramsKeyMap[key];
-    };
+    }
 
-    p._getStatementIndex = function(key) {
+    _getStatementIndex(key) {
         if (!this._schema) {
             this._schema = Entry.block[this.type];
         }
         return this._schema.statementsKeyMap[key];
-    };
+    }
 
-    p.die = function() {
+    die() {
         this.block = null;
         return Entry.STATIC.BREAK;
-    };
-})(Entry.Scope.prototype);
+    }
+}
+
+Entry.Scope = Scope;
