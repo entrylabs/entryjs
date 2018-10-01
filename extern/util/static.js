d3f063caf72f237456551877bf17c60215d103dc
Kimsorang <37480552+Kimsorang@users.noreply.github.com>
2018-10-01 15:27:23 +0900

    수정완료

--- extern/util/static.js
+++ extern/util/static.js
@@ -1335,6 +1335,28 @@ EntryStatic.getAllBlocks = function () {
                 "dash_eye",
                 "dash_animation",
                 //endregion dash
+
+                //Codingmachine Blocks
+                "codingmachine_led",
+                "codingmachine_tune",
+                "codingmachine_motor",
+                "codingmachine_joystick",
+                "codingmachine_button",
+                "codingmachine_gyro",
+                "codingmachine_ultrasonic",
+                "codingmachine_connect",
+                "codingmachine_ready",
+                "codingmachine_throttle",
+                "codingmachine_altitude",
+                "codingmachine_rollpitch",
+                "codingmachine_yaw",
+                "codingmachine_emergency",
+                "codingmachine_digital_out",
+                "codingmachine_digital_in",
+                "codingmachine_analog_in",
+                "codingmachine_digital_pwm",
+                "codingmachine_servo",
+                //Added 2018-09-24
             ]
         }
     ]
@@ -1581,4 +1603,4 @@ if (typeof exports == "object") {
     exports.getAllBlocks = EntryStatic.getAllBlocks;
     exports.getCategoryByBlock = EntryStatic.getCategoryByBlock;
     exports.EntryStatic = EntryStatic;
-}
+}// JavaScript source code
