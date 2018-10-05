92854f6480492e2ac678d4b177ff59bae687102a
jykim <kimorkim@gmail.com>
2018-10-05 18:28:58 +0900

    file color class 형식으로 변경

--- src/playground/field/color.js
+++ src/playground/field/color.js
@@ -5,35 +5,33 @@
 /*
  *
  */
-Entry.FieldColor = function(content, blockView, index) {
-    this._block = blockView.block;
-    this._blockView = blockView;
+Entry.FieldColor = class FieldColor extends Entry.Field{
+    constructor(content, blockView, index) {
+        this._block = blockView.block;
+        this._blockView = blockView;
 
-    this.box = new Entry.BoxModel();
+        this.box = new Entry.BoxModel();
 
-    this.svgGroup = null;
+        this.svgGroup = null;
 
-    this._contents = content;
-    this._index = index;
-    this._position = content.position;
-    this._fontSize = content.fontSize || blockView.getSkeleton().fontSize || 10;
-    this._color =
-        content.color ||
-        this._block.getSchema().fontColor ||
-        blockView.getSkeleton().color ||
-        'black';
-    this.key = content.key;
-    this.setValue(this.getValue() || '#FF0000');
-    this._CONTENT_HEIGHT = this.getContentHeight();
-    this._CONTENT_WIDTH = this.getContentWidth();
+        this._contents = content;
+        this._index = index;
+        this._position = content.position;
+        this._fontSize = content.fontSize || blockView.getSkeleton().fontSize || 10;
+        this._color =
+            content.color ||
+            this._block.getSchema().fontColor ||
+            blockView.getSkeleton().color ||
+            'black';
+        this.key = content.key;
+        this.setValue(this.getValue() || '#FF0000');
+        this._CONTENT_HEIGHT = this.getContentHeight();
+        this._CONTENT_WIDTH = this.getContentWidth();
 
-    this.renderStart();
-};
+        this.renderStart();
+    };
 
-Entry.Utils.inherit(Entry.Field, Entry.FieldColor);
-
-(function(p) {
-    p.renderStart = function() {
+    renderStart() {
         if (this.svgGroup) {
             $(this.svgGroup).remove();
         }
@@ -112,7 +110,7 @@ Entry.Utils.inherit(Entry.Field, Entry.FieldColor);
         });
     };
 
-    p.renderOptions = function() {
+    renderOptions() {
         var that = this;
 
         this._attachDisposeEvent();
@@ -167,7 +165,7 @@ Entry.Utils.inherit(Entry.Field, Entry.FieldColor);
         this.optionDomCreated();
     };
 
-    p.applyValue = function(value) {
+    applyValue(value) {
         if (this.value == value) {
             return;
         }
@@ -182,102 +180,102 @@ Entry.Utils.inherit(Entry.Field, Entry.FieldColor);
         }
     };
 
-    p.getContentWidth = function() {
+    getContentWidth() {
         return 22;
     };
-})(Entry.FieldColor.prototype);
 
-Entry.FieldColor.getWidgetColorList = function() {
-    return [
-        [
-            '#FFFFFF',
-            '#CCCCCC',
-            '#C0C0C0',
-            '#999999',
-            '#666666',
-            '#333333',
-            '#000000',
-        ],
-        [
-            '#FFCCCC',
-            '#FF6666',
-            '#FF0000',
-            '#CC0000',
-            '#990000',
-            '#660000',
-            '#330000',
-        ],
-        [
-            '#FFCC99',
-            '#FF9966',
-            '#FF9900',
-            '#FF6600',
-            '#CC6600',
-            '#993300',
-            '#663300',
-        ],
-        [
-            '#FFFF99',
-            '#FFFF66',
-            '#FFCC66',
-            '#FFCC33',
-            '#CC9933',
-            '#996633',
-            '#663333',
-        ],
-        [
-            '#FFFFCC',
-            '#FFFF33',
-            '#FFFF00',
-            '#FFCC00',
-            '#999900',
-            '#666600',
-            '#333300',
-        ],
-        [
-            '#99FF99',
-            '#66FF99',
-            '#33FF33',
-            '#33CC00',
-            '#009900',
-            '#006600',
-            '#003300',
-        ],
-        [
-            '#99FFFF',
-            '#33FFFF',
-            '#66CCCC',
-            '#00CCCC',
-            '#339999',
-            '#336666',
-            '#003333',
-        ],
-        [
-            '#CCFFFF',
-            '#66FFFF',
-            '#33CCFF',
-            '#3366FF',
-            '#3333FF',
-            '#000099',
-            '#000066',
-        ],
-        [
-            '#CCCCFF',
-            '#9999FF',
-            '#6666CC',
-            '#6633FF',
-            '#6609CC',
-            '#333399',
-            '#330099',
-        ],
-        [
-            '#FFCCFF',
-            '#FF99FF',
-            '#CC66CC',
-            '#CC33CC',
-            '#993399',
-            '#663366',
-            '#330033',
-        ],
-    ];
-};
+    static getWidgetColorList() {
+        return [
+            [
+                '#FFFFFF',
+                '#CCCCCC',
+                '#C0C0C0',
+                '#999999',
+                '#666666',
+                '#333333',
+                '#000000',
+            ],
+            [
+                '#FFCCCC',
+                '#FF6666',
+                '#FF0000',
+                '#CC0000',
+                '#990000',
+                '#660000',
+                '#330000',
+            ],
+            [
+                '#FFCC99',
+                '#FF9966',
+                '#FF9900',
+                '#FF6600',
+                '#CC6600',
+                '#993300',
+                '#663300',
+            ],
+            [
+                '#FFFF99',
+                '#FFFF66',
+                '#FFCC66',
+                '#FFCC33',
+                '#CC9933',
+                '#996633',
+                '#663333',
+            ],
+            [
+                '#FFFFCC',
+                '#FFFF33',
+                '#FFFF00',
+                '#FFCC00',
+                '#999900',
+                '#666600',
+                '#333300',
+            ],
+            [
+                '#99FF99',
+                '#66FF99',
+                '#33FF33',
+                '#33CC00',
+                '#009900',
+                '#006600',
+                '#003300',
+            ],
+            [
+                '#99FFFF',
+                '#33FFFF',
+                '#66CCCC',
+                '#00CCCC',
+                '#339999',
+                '#336666',
+                '#003333',
+            ],
+            [
+                '#CCFFFF',
+                '#66FFFF',
+                '#33CCFF',
+                '#3366FF',
+                '#3333FF',
+                '#000099',
+                '#000066',
+            ],
+            [
+                '#CCCCFF',
+                '#9999FF',
+                '#6666CC',
+                '#6633FF',
+                '#6609CC',
+                '#333399',
+                '#330099',
+            ],
+            [
+                '#FFCCFF',
+                '#FF99FF',
+                '#CC66CC',
+                '#CC33CC',
+                '#993399',
+                '#663366',
+                '#330033',
+            ],
+        ];
+    };    
+}
