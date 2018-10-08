781116cad8f34ef5e84a09e2085ca7fe9932f4f7
youngduk.park <youngduk.park@nhn.com>
2018-10-08 10:14:26 +0900

    edit observer.js transfer class

--- src/core/observer.js
+++ src/core/observer.js
@@ -4,19 +4,22 @@
  * Entry Observer object Constructor
  * @param {object} obj
  */
-Entry.Observer = function(parent, object, funcName, attrs) {
-    this.parent = parent;
-    this.object = object;
-    this.funcName = funcName;
-    this.attrs = attrs;
-    parent.push(this);
-};
 
-(function(p) {
-    p.destroy = function() {
-        var parent = this.parent;
-        var index = parent.indexOf(this);
-        if (index > -1) parent.splice(index, 1);
+Entry.Observer = class Observer {
+    constructor(parent, object, funcName, attrs) {
+        this.parent = parent;
+        this.object = object;
+        this.funcName = funcName;
+        this.attrs = attrs;
+        parent.push(this);
+    }
+
+    destroy = function() {
+        const parent = this.parent;
+        const index = parent.indexOf(this);
+        if (index > -1) {
+            parent.splice(index, 1);
+        }
         return this;
     };
-})(Entry.Observer.prototype);
+};
