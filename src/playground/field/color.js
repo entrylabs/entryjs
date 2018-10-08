895be45897149e35ef56898603f9d67c84df9b88
jykim <kimorkim@gmail.com>
2018-10-08 18:12:16 +0900

    add color picker

--- src/playground/field/color.js
+++ src/playground/field/color.js
@@ -2,11 +2,14 @@
  */
 'use strict';
 
+import EntryTool from 'entry-tool';
+
 /*
  *
  */
-Entry.FieldColor = class FieldColor extends Entry.Field{
+Entry.FieldColor = class FieldColor extends Entry.Field {
     constructor(content, blockView, index) {
+        super(content, blockView, index);
         this._block = blockView.block;
         this._blockView = blockView;
 
@@ -29,7 +32,7 @@ Entry.FieldColor = class FieldColor extends Entry.Field{
         this._CONTENT_WIDTH = this.getContentWidth();
 
         this.renderStart();
-    };
+    }
 
     renderStart() {
         if (this.svgGroup) {
@@ -59,10 +62,7 @@ Entry.FieldColor = class FieldColor extends Entry.Field{
                 fill: this._color,
             });
 
-            this.textElement.textContent = this._convert(
-                this.getValue(),
-                this.getValue()
-            );
+            this.textElement.textContent = this._convert(this.getValue(), this.getValue());
             var bBox = this.textElement.getBoundingClientRect();
             WIDTH = bBox.width + 12;
             HEIGHT = bBox.height;
@@ -92,8 +92,8 @@ Entry.FieldColor = class FieldColor extends Entry.Field{
                 y: y,
                 rx: 2,
                 ry: 2,
-                width: 20,//WIDTH,
-                height: 20,//HEIGHT,
+                width: 20, //WIDTH,
+                height: 20, //HEIGHT,
                 fill: this.getValue(),
                 stroke: '#fff',
                 'stroke-width': '1',
@@ -108,51 +108,39 @@ Entry.FieldColor = class FieldColor extends Entry.Field{
             width: WIDTH,
             height: HEIGHT,
         });
-    };
+    }
+
+    _attachDisposeEvent(func) {
+        let action = func;
+        if(!action) {
+            action = (skipCommand) => {
+                this.applyValue(this.colorPicker.getData('color'));
+                this.destroyOption(skipCommand);
+                this._selectBlockView();
+            };
+        }
+        this.disposeEvent = Entry.disposeEvent.attach(this, action);
+    }
 
     renderOptions() {
-        var that = this;
-
         this._attachDisposeEvent();
 
-        this.optionGroup = Entry.Dom('table', {
-            class: 'entry-widget-color-table',
+        this.optionGroup = Entry.Dom('div', {
+            class: 'entry-color-picker',
             parent: $('body'),
         });
 
-        this.optionGroup.bind('mousedown touchstart', (e) =>
-            e.stopPropagation()
-        );
-
-        this.optionGroup.on('mouseup', 'td', function(e) {
-            that.applyValue(this.getAttribute('data-color-value'));
-            that.destroyOption();
-            that._selectBlockView();
+        this.colorPicker = new EntryTool({
+            type: 'colorPicker',
+            data: {
+                color: this.getValue(),
+            },
+            container: this.optionGroup[0],
         });
 
-        var fragment = document.createDocumentFragment();
-        Entry.FieldColor.getWidgetColorList().forEach((row) => {
-            var tr = Entry.Dom('tr', {
-                class: 'entry-widget-color-row',
-            });
+        this.optionGroup.bind('mousedown touchstart keyup keydown', (e) => e.stopPropagation());
 
-            fragment.appendChild(tr[0]);
-
-            row.forEach((color) => {
-                Entry.Dom('td', {
-                    class: 'entry-widget-color-cell',
-                    parent: tr,
-                })
-                    .css({
-                        'background-color': color,
-                    })
-                    .attr({
-                        'data-color-value': color,
-                    });
-            });
-        });
-
-        this.optionGroup[0].appendChild(fragment);
+        // this.optionGroup[0].appendChild(fragment);
 
         var { x, y } = this.getAbsolutePosFromDocument();
         y += this.box.height / 2 + 1;
@@ -163,7 +151,7 @@ Entry.FieldColor = class FieldColor extends Entry.Field{
         });
 
         this.optionDomCreated();
-    };
+    }
 
     applyValue(value) {
         if (this.value == value) {
@@ -178,104 +166,24 @@ Entry.FieldColor = class FieldColor extends Entry.Field{
             value = this.getValue();
             this.textElement.textContent = this._convert(value, value);
         }
-    };
+    }
 
     getContentWidth() {
         return 22;
-    };
+    }
 
     static getWidgetColorList() {
         return [
-            [
-                '#FFFFFF',
-                '#CCCCCC',
-                '#C0C0C0',
-                '#999999',
-                '#666666',
-                '#333333',
-                '#000000',
-            ],
-            [
-                '#FFCCCC',
-                '#FF6666',
-                '#FF0000',
-                '#CC0000',
-                '#990000',
-                '#660000',
-                '#330000',
-            ],
-            [
-                '#FFCC99',
-                '#FF9966',
-                '#FF9900',
-                '#FF6600',
-                '#CC6600',
-                '#993300',
-                '#663300',
-            ],
-            [
-                '#FFFF99',
-                '#FFFF66',
-                '#FFCC66',
-                '#FFCC33',
-                '#CC9933',
-                '#996633',
-                '#663333',
-            ],
-            [
-                '#FFFFCC',
-                '#FFFF33',
-                '#FFFF00',
-                '#FFCC00',
-                '#999900',
-                '#666600',
-                '#333300',
-            ],
-            [
-                '#99FF99',
-                '#66FF99',
-                '#33FF33',
-                '#33CC00',
-                '#009900',
-                '#006600',
-                '#003300',
-            ],
-            [
-                '#99FFFF',
-                '#33FFFF',
-                '#66CCCC',
-                '#00CCCC',
-                '#339999',
-                '#336666',
-                '#003333',
-            ],
-            [
-                '#CCFFFF',
-                '#66FFFF',
-                '#33CCFF',
-                '#3366FF',
-                '#3333FF',
-                '#000099',
-                '#000066',
-            ],
-            [
-                '#CCCCFF',
-                '#9999FF',
-                '#6666CC',
-                '#6633FF',
-                '#6609CC',
-                '#333399',
-                '#330099',
-            ],
-            [
-                '#FFCCFF',
-                '#FF99FF',
-                '#CC66CC',
-                '#CC33CC',
-                '#993399',
-                '#663366',
-                '#330033',
-            ],
+            ['#FFFFFF', '#CCCCCC', '#C0C0C0', '#999999', '#666666', '#333333', '#000000'],
+            ['#FFCCCC', '#FF6666', '#FF0000', '#CC0000', '#990000', '#660000', '#330000'],
+            ['#FFCC99', '#FF9966', '#FF9900', '#FF6600', '#CC6600', '#993300', '#663300'],
+            ['#FFFF99', '#FFFF66', '#FFCC66', '#FFCC33', '#CC9933', '#996633', '#663333'],
+            ['#FFFFCC', '#FFFF33', '#FFFF00', '#FFCC00', '#999900', '#666600', '#333300'],
+            ['#99FF99', '#66FF99', '#33FF33', '#33CC00', '#009900', '#006600', '#003300'],
+            ['#99FFFF', '#33FFFF', '#66CCCC', '#00CCCC', '#339999', '#336666', '#003333'],
+            ['#CCFFFF', '#66FFFF', '#33CCFF', '#3366FF', '#3333FF', '#000099', '#000066'],
+            ['#CCCCFF', '#9999FF', '#6666CC', '#6633FF', '#6609CC', '#333399', '#330099'],
+            ['#FFCCFF', '#FF99FF', '#CC66CC', '#CC33CC', '#993399', '#663366', '#330033'],
         ];
-    };    
-}
+    }
+};
