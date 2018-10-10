086c6fd89ce3ee76694c3cc1005a71e999a1bd9f
junbae.park <junbae.park@nts-corp.com>
2018-10-10 19:09:51 +0900

    [etc] 별거아닌커밋

--- src/class/pixi/__testfiles/articleloader.js
+++ src/class/pixi/__testfiles/articleloader.js
@@ -1,18 +1,11 @@
-import { ProjectParser } from '../atlas/ProjectParser';
 
 var article;
 // article = require("./pacuru.json");
 // article = require("./brush.json");
 article = require("./scene.json");
 
-ProjectParser.parse(article.objects);
-/*
+
 window.loadArticle = ()=>{
-    Entry.clearProject();
     Entry.loadProject(article);
 };
-if(article) {
-    setTimeout(window.loadArticle, 2000);
-}
-*/
 
