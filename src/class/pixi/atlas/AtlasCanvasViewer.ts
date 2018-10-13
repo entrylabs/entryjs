63630f1b9720daf59bcce6a9d92d6f5bdb82b330
junbae.park <junbae.park@nts-corp.com>
2018-10-13 15:49:59 +0900

    [TexureAtlas] <img> 의 key를 picID 에서 path 로 변경 중~!

--- src/class/pixi/atlas/AtlasCanvasViewer.ts
+++ src/class/pixi/atlas/AtlasCanvasViewer.ts
@@ -2,19 +2,20 @@
  * BaseTexture 의 canvas 에 drawing이 잘 되는지 확인하기 위한 debug 클라스.
  */
 
-let style = `<style>
+let style = (viewerHeight:number)=>{return `<style>
     .atlas-canvas-container {
         position: absolute;
         width: 100%;
         pointer-events: none;
     }
     .atlas-canvas {
-        width: auto; height: 100px; float: right;
+        width: auto; height: ${viewerHeight}px; float: right;
     }
-</style>
-`;
+</style>`;
+};
 
 let dom = `<div class='atlas-canvas-container'></div>`;
+
 declare let $:any;
 export class AtlasCanvasViewer {
 
@@ -22,8 +23,8 @@ export class AtlasCanvasViewer {
     private $container:any;
     private attached:boolean;
 
-    constructor() {
-        this.$style = $(style);
+    constructor(viewerHeight:number=200) {
+        this.$style = $(style(viewerHeight));
         this.$container = $(dom);
     }
 
@@ -37,10 +38,10 @@ export class AtlasCanvasViewer {
         }, 500);
     }
 
-    add(c:any) {
-        this.attachToBody();
+    add(c:HTMLCanvasElement) {
         var isCanvas = c instanceof HTMLCanvasElement;
         if(!isCanvas) return;
+        this.attachToBody();
         var cc = $(c);
         this.$container.append(cc);
         cc.addClass("atlas-canvas");
