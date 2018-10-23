c1a2cf6dd2ce95c9f5b7b01f79d6bef2835422dd
extracold1209 <extracold1209@naver.com>
2018-10-23 15:54:15 +0900

    static.js 에 hardwareBlock 을 동적으로 추가할 수 있도록 구조변경
    - static 에 등록되지 않았던 하드웨어 블록도 불러와지는 문제가 있음
    (블록메뉴에는 미추가이나 존재하는 하드웨어 값 블록의 경우)

--- src/playground/blocks/index.js
+++ src/playground/blocks/index.js
@@ -119,17 +119,19 @@ Entry.HARDWARE_LIST = {
     '26.1': Entry.Codingmachine,
 };
 
-const startBlock = require('./block_start');
-const flowBlock = require('./block_flow');
-const movingBlock = require('./block_moving');
-const looksBlock = require('./block_looks');
-const brushBlock = require('./block_brush');
-const textBlock = require('./block_text');
-const soundBlock = require('./block_sound');
-const judgementBlock = require('./block_judgement');
-const calcBlock = require('./block_calc');
-const variableBlock = require('./block_variable');
-const funcBlock = require('./block_func');
+const basicBlockList = [
+    require('./block_start'),
+    require('./block_flow'),
+    require('./block_moving'),
+    require('./block_looks'),
+    require('./block_brush'),
+    require('./block_text'),
+    require('./block_sound'),
+    require('./block_judgement'),
+    require('./block_calc'),
+    require('./block_variable'),
+    require('./block_func'),
+];
 
 Entry.EXPANSION_BLOCK = {};
 require('./block_expansion_weather');
@@ -139,36 +141,46 @@ require('./block_expansion_translate');
 Entry.EXPANSION_BLOCK_LIST = {
     weather: Entry.Expansion_Weather,
     festival: Entry.EXPANSION_BLOCK.festival,
-    translate : Entry.EXPANSION_BLOCK.translate
+    translate: Entry.EXPANSION_BLOCK.translate,
 };
 
 function getBlockObject(items) {
     const blockObject = {};
-    items.forEach((item)=> {
-        if('getBlocks' in item) {
+    items.forEach((item) => {
+        if ('getBlocks' in item) {
             Object.assign(blockObject, item.getBlocks());
         }
     });
     return blockObject;
 }
 
+/**
+ * 하드웨어 블록을 EntryStatic 에 등록한다.
+ * 하드웨어 블록에만 사용하는 이유는,
+ * 기존 블록은 legacy 블록이 존재하기 때문에 전부 등록하면 안되기 때문이다.
+ * 또한 값블록으로서만 사용하는 블록이 블록메뉴에 따로 나타나게 될 수 있다.
+ *
+ * block 자체에 ignore: true 프로퍼티가 존재하는 경우 스킵한다.
+ *
+ * @param {Object} blockList
+ * @see getBlockList
+ * @return {void}
+ */
+function registerHardwareBlockToStatic(blockList) {
+    Object.keys(blockList).forEach((block) => {
+        if (block.ignore !== true &&
+            EntryStatic.DynamicHardwareBlocks.indexOf(block) === -1) {
+            EntryStatic.DynamicHardwareBlocks.push(block);
+        }
+    });
+}
+
 module.exports = {
     getBlocks() {
-        const basicBlockList = [
-            startBlock,
-            flowBlock,
-            movingBlock,
-            looksBlock,
-            brushBlock,
-            textBlock,
-            soundBlock,
-            judgementBlock,
-            calcBlock,
-            variableBlock,
-            funcBlock
-        ];
-        const expansionBlockList = Object.values(Entry.EXPANSION_BLOCK_LIST);
-        const hardwareList = Object.values(Entry.HARDWARE_LIST);
-        return getBlockObject(basicBlockList.concat(hardwareList).concat(expansionBlockList));
-    }
-}
+        const basicAndExpansionBlockObjectList = getBlockObject(basicBlockList.concat(Object.values(Entry.EXPANSION_BLOCK_LIST)));
+        const hardwareObjectList = getBlockObject(Object.values(Entry.HARDWARE_LIST));
+        registerHardwareBlockToStatic(hardwareObjectList);
+
+        return Object.assign({}, basicAndExpansionBlockObjectList, hardwareObjectList);
+    },
+};
