241217eb08ab9b0c98cdf4509e412e86cd5e82cc
youngduk.park <youngduk.park@nhn.com>
2018-11-07 22:12:09 +0900

    edit comment resize & refactoring

--- src/playground/comment.js
+++ src/playground/comment.js
@@ -23,18 +23,16 @@ Entry.Comment = class Comment {
         if (!board.svgCommentGroup) {
             return;
         }
-        const { comment, view } = block;
+        const { view } = block;
         const { svgGroup } = view || {};
         this._block = block;
         this._board = board;
         this._blockView = view;
         this.value = '';
         if (svgGroup && !(board instanceof Entry.BlockMenu)) {
-            console.time('a');
             this.createComment();
             this.startRender();
             this.addControl();
-            console.timeEnd('a');
         }
 
         this.observe(this, 'updateOpacity', ['visible'], false);
@@ -56,14 +54,18 @@ Entry.Comment = class Comment {
         return this.board.scale || 1;
     }
 
-    _getFontSize() {
+    get fontSize() {
         return this.scale * 10;
     }
 
-    _getTextareaPath() {
+    get textAreaPath() {
         let d = '';
-        for (let y = 14; y < this.height - this.titleHeight - 4; y += 16) {
-            d += `M6,${y} H${this.width - 12} `;
+        let { width, height } = this;
+        width = Math.max(width, 100);
+        height = Math.max(height, 100);
+
+        for (let y = 14; y < height - this.titleHeight - 4; y += 16) {
+            d += `M6,${y} H${width - 12} `;
         }
         return d;
     }
@@ -84,6 +86,9 @@ Entry.Comment = class Comment {
         this.mouseDown = this.mouseDown.bind(this);
         this.mouseMove = this.mouseMove.bind(this);
         this.mouseUp = this.mouseUp.bind(this);
+        this.resizeMouseDown = this.resizeMouseDown.bind(this);
+        this.resizeMouseMove = this.resizeMouseMove.bind(this);
+        this.resizeMouseUp = this.resizeMouseUp.bind(this);
     }
 
     startRender() {
@@ -95,86 +100,119 @@ Entry.Comment = class Comment {
             this._path = this.svgGroup.elem('defs').elem('path');
             this._text = this.svgGroup.elem('text');
             this._textpath = this._text.elem('textPath');
+            this._resizeArea = this.svgGroup.elem('rect');
+            this._resizeArrow = this.svgGroup.elem('image');
 
-            const { width: parentWidth } = this.pathGroup.getBBox();
-            const { topFieldHeight, height } = this._blockView;
-            const parentHeight = topFieldHeight || height;
-            const { titleHeight, defaultLineLength } = this;
-            const x = defaultLineLength + parentWidth;
-            const y = parentHeight / 2 - titleHeight / 2;
-
-            this.set({
-                x,
-                y,
-                parentWidth,
-                parentHeight,
-            });
-
-            this._comment.attr({
-                stroke: '#EDA913',
-                fill: '#FFDA85',
-                rx: '4',
-            });
-
-            this._title.attr({
-                stroke: '#EDA913',
-                fill: '#FBB315',
-                rx: '4',
-            });
-
-            this._line.attr({
-                style: 'stroke:#eda913;stroke-width:2',
-            });
-
-            this._path.attr({
-                id: `${this.id}`,
-                stroke: 'red',
-            });
-
-            this._text.attr({
-                'font-size': 10,
-            });
-
-            this._textpath.attr({
-                href: `#${this.id}`,
-            });
-
-            this._textpath.textContent =
-                '메모입니다. 메모입니다.메모입니다.메모입니다.메모입니다.메모입니다메모입니다. 메모입니다.메모입니다.메모입니다.메모입니다.메모입니다.메모입니다. 메모입니다.메모입니다.메모입니다.메모입니다.메모입니다.';
             this.canRender = true;
+            this.setInitSchema();
+            this.setFrame();
         }
         this.setPosition();
     }
 
-    setPosition() {
+    setInitSchema() {
+        const { width: parentWidth } = this.pathGroup.getBBox();
+        const { topFieldHeight, height } = this._blockView;
+        const parentHeight = topFieldHeight || height;
+        const { titleHeight, defaultLineLength } = this;
+        const x = defaultLineLength + parentWidth;
+        const y = parentHeight / 2 - titleHeight / 2;
+
+        this.set({
+            x,
+            y,
+            parentWidth,
+            parentHeight,
+        });
+    }
+
+    setFrame() {
+        this._comment.attr({
+            stroke: '#EDA913',
+            fill: '#FFDA85',
+            rx: '4',
+        });
+
         this._title.attr({
-            x: this.x,
-            y: this.y,
-            width: this.width,
+            stroke: '#EDA913',
+            fill: '#FBB315',
+            rx: '4',
+        });
+
+        this._line.attr({
+            style: 'stroke:#eda913;stroke-width:2',
+        });
+
+        this._path.attr({
+            id: `${this.id}`,
+            stroke: 'red',
+        });
+
+        this._text.attr({
+            'font-size': 10,
+        });
+
+        this._textpath.attr({
+            href: `#${this.id}`,
+        });
+
+        this._textpath.textContent =
+            '메모입니다. 메모입니다.메모입니다.메모입니다.메모입니다.메모입니다메모입니다.' +
+            '메모입니다.메모입니다.메모입니다.메모입니다.메모입니다.메모입니다. 메모입니다.' +
+            '메모입니다.메모입니다.메모입니다.메모입니다.';
+
+        const path = `${Entry.mediaFilePath}block_icon/comment/`;
+        this._resizeArea.attr({
+            width: 20,
+            height: 20,
+            fill: 'transparent',
+            class: 'entry-comment-resize-arrow',
+        });
+
+        this._resizeArrow.attr({
+            href: `${path}resize_arrow.svg`,
+        });
+    }
+
+    setPosition() {
+        const { x, y } = this;
+        const width = Math.max(this.width, 100);
+        const height = Math.max(this.height, 100);
+
+        this._title.attr({
+            x,
+            y,
+            width,
             height: this.titleHeight,
         });
 
         this._comment.attr({
-            x: this.x,
-            y: this.y,
-            width: this.width,
-            height: this.height,
+            x,
+            y,
+            width,
+            height,
         });
 
         this._line.attr({
             x1: this.parentWidth,
             y1: this.parentHeight / 2,
-            x2: this.x + this.width / 2,
-            y2: this.y + this.titleHeight / 2,
+            x2: x + width / 2,
+            y2: y + this.titleHeight / 2,
         });
 
         this._path.attr({
-            transform: `translate(${this.x}, ${this.y + this.titleHeight})`,
-            d: this._getTextareaPath(),
+            transform: `translate(${x}, ${y + this.titleHeight})`,
+            d: this.textAreaPath,
         });
 
-        this._text.attr({
-            // transform: `translate(${this.x}, ${this.y + this.titleHeight})`,
+        this._resizeArea.attr({
+            x: x + width - 20,
+            y: y + height - 20,
+        });
+
+        this._resizeArrow.attr({
+            x: x + width - 14,
+            y: y + height - 14,
         });
     }
 
@@ -219,43 +257,70 @@ Entry.Comment = class Comment {
         return this.moveTo(this.x + x, this.y + y);
     }
 
+    resize(x, y) {
+        this.set({
+            width: this.width + x,
+            height: this.height + y,
+        });
+        this.setPosition();
+    }
+
+    setDragInstance(e) {
+        const { width: parentWidth } = this.pathGroup.getBBox();
+        const { topFieldHeight, height } = this._blockView;
+        const parentHeight = topFieldHeight || height;
+        const mouseEvent = Entry.Utils.convertMouseEvent(e);
+        const matrix = this.svgGroup.getCTM();
+        const { x, y } = Entry.GlobalSvg.getRelativePoint(matrix);
+        this.mouseDownCoordinate = {
+            x: mouseEvent.pageX,
+            y: mouseEvent.pageY,
+            parentX: x,
+            parentY: y,
+        };
+        this.dragInstance = new Entry.DragInstance({
+            startX: x / this.scale + parentWidth,
+            startY: y / this.scale + parentHeight / 2,
+            offsetX: mouseEvent.pageX,
+            offsetY: mouseEvent.pageY,
+            mode: true,
+        });
+    }
+
+    bindDomEvent(mouseMove, mouseUp) {
+        document.onmousemove = mouseMove;
+        document.ontouchmove = mouseMove;
+        document.onmouseup = mouseUp;
+        document.ontouchend = mouseUp;
+    }
+
+    removeDomEvent() {
+        document.onmousemove = undefined;
+        document.ontouchmove = undefined;
+        document.onmouseup = undefined;
+        document.ontouchend = undefined;
+    }
+
+    getMouseMoveDiff(mouseEvent) {
+        return Math.sqrt(
+            Math.pow(mouseEvent.pageX - this.mouseDownCoordinate.x, 2) +
+                Math.pow(mouseEvent.pageY - this.mouseDownCoordinate.y, 2)
+        );
+    }
+
     mouseDown(e) {
         e.preventDefault();
         e.stopPropagation();
 
-        if (this.isEditing) {
-            this.destroyTextArea();
-        }
-        const scale = this.scale;
+        this.isEditing && this.destroyTextArea();
+
         if (
             (e.button === 0 || (e.originalEvent && e.originalEvent.touches)) &&
             !this._board.readOnly
         ) {
-            const { width: parentWidth } = this.pathGroup.getBBox();
-            const { topFieldHeight, height } = this._blockView;
-            const parentHeight = topFieldHeight || height;
-            const mouseEvent = Entry.Utils.convertMouseEvent(e);
-            const matrix = this.svgGroup.getCTM();
-            const { x, y } = Entry.GlobalSvg.getRelativePoint(matrix);
-            const { mouseMove, mouseUp } = this;
-            this.mouseDownCoordinate = {
-                x: mouseEvent.pageX,
-                y: mouseEvent.pageY,
-                parentX: x,
-                parentY: y,
-            };
-            document.onmousemove = mouseMove;
-            document.ontouchmove = mouseMove;
-            document.onmouseup = mouseUp;
-            document.ontouchend = mouseUp;
-            this.dragInstance = new Entry.DragInstance({
-                startX: x / scale + parentWidth,
-                startY: y / scale + parentHeight / 2,
-                offsetX: mouseEvent.pageX,
-                offsetY: mouseEvent.pageY,
-                mode: true,
-            });
+            this.setDragInstance(e);
             this.dragMode = Entry.DRAG_MODE_MOUSEDOWN;
+            this.bindDomEvent(this.mouseMove, this.mouseUp);
         }
     }
 
@@ -264,25 +329,22 @@ Entry.Comment = class Comment {
         e.stopPropagation();
         this.isEditing = false;
         const mouseEvent = Entry.Utils.convertMouseEvent(e);
-        const diff = Math.sqrt(
-            Math.pow(mouseEvent.pageX - this.mouseDownCoordinate.x, 2) +
-                Math.pow(mouseEvent.pageY - this.mouseDownCoordinate.y, 2)
-        );
-        if (this.dragMode == Entry.DRAG_MODE_DRAG || diff > Entry.BlockView.DRAG_RADIUS) {
+        if (
+            this.dragMode === Entry.DRAG_MODE_DRAG ||
+            this.getMouseMoveDiff(mouseEvent) > Entry.BlockView.DRAG_RADIUS
+        ) {
             this.set({ visible: false });
             const workspaceMode = this.board.workspace.getMode();
 
             const dragInstance = this.dragInstance;
             const scale = this.scale;
-            if (this.dragMode != Entry.DRAG_MODE_DRAG) {
+            if (this.dragMode !== Entry.DRAG_MODE_DRAG) {
                 this.dragMode = Entry.DRAG_MODE_DRAG;
                 Entry.GlobalSvg.setComment(this, workspaceMode);
             }
             this.moveBy(
                 (mouseEvent.pageX - dragInstance.offsetX) / scale,
-                (mouseEvent.pageY - dragInstance.offsetY) / scale,
-                false,
-                true
+                (mouseEvent.pageY - dragInstance.offsetY) / scale
             );
 
             dragInstance.set({
@@ -297,35 +359,35 @@ Entry.Comment = class Comment {
         e.preventDefault();
         e.stopPropagation();
         if (this.dragMode === Entry.DRAG_MODE_MOUSEDOWN) {
-            this.isEditing ? this.isEditing = false : this.renderTextArea();
+            this.isEditing ? (this.isEditing = false) : this.renderTextArea();
         }
-        this.dragMode = Entry.DRAG_MODE_NONE;
-        document.onmousemove = undefined;
-        document.ontouchmove = undefined;
-        document.onmouseup = undefined;
-        document.ontouchend = undefined;
-        this.board.set({ dragBlock: null });
         Entry.GlobalSvg.remove();
+        this.dragMode = Entry.DRAG_MODE_NONE;
+        this.board.set({ dragBlock: null });
         this.set({ visible: true });
         this.setPosition();
+        this.removeDomEvent();
         delete this.mouseDownCoordinate;
         delete this.dragInstance;
     }
 
     addControl() {
-        const dom = this.svgGroup;
-        dom.onmousedown = this.mouseDown;
-        dom.ontouchstart = this.mouseDown;
+        this._comment.onmousedown = this.mouseDown;
+        this._comment.ontouchstart = this.mouseDown;
+        this._title.onmousedown = this.mouseDown;
+        this._title.ontouchstart = this.mouseDown;
+        this._resizeArea.onmousedown = this.resizeMouseDown;
+        this._resizeArea.ontouchstart = this.resizeMouseDown;
     }
 
-    updateOpacity = function() {
+    updateOpacity() {
         if (this.visible === false) {
             this.svgGroup.attr({ opacity: 0 });
         } else {
             this.svgGroup.removeAttr('opacity');
             this.setPosition();
         }
-    };
+    }
 
     isReadOnly() {
         return this.readOnly;
@@ -360,6 +422,7 @@ Entry.Comment = class Comment {
 
     renderTextArea() {
         this.isEditing = true;
+        const { top, left } = this._comment.getBoundingClientRect();
         this.event = Entry.disposeEvent.attach(this, () => {
             this._textpath.textContent = this.textArea.val();
             this.destroyTextArea();
@@ -369,7 +432,21 @@ Entry.Comment = class Comment {
             class: 'entry-widget-textarea',
             parent: $('body'),
         });
+        this.bindDomEventTextArea();
         this.textArea.val(this._textpath.textContent);
+        this.textArea.css({
+            left,
+            top: this.titleHeight * this.scale + top,
+            'font-size': `${this.fontSize}px`,
+            width: this.width * this.scale,
+            height: (this.height - this.titleHeight) * this.scale,
+            'border-radius': `0 0 ${4 * this.scale}px ${4 * this.scale}px`,
+            padding: `${3 * this.scale}px ${6 * this.scale}px`,
+        });
+        this.textArea.focus && this.textArea.focus();
+    }
+
+    bindDomEventTextArea() {
         this.textArea.on('mousedown', (e) => {
             e.stopPropagation();
         });
@@ -387,17 +464,6 @@ Entry.Comment = class Comment {
                 this.isEditing = false;
             }
         });
-        const { top, left } = this._comment.getBoundingClientRect();
-        this.textArea.css({
-            left,
-            top: this.titleHeight * this.scale + top,
-            'font-size': `${this._getFontSize()}px`,
-            width: this.width * this.scale,
-            height: (this.height - this.titleHeight) * this.scale,
-            'border-radius': `0 0 ${4 * this.scale}px ${4 * this.scale}px`,
-            padding: `${3 * this.scale}px ${6 * this.scale}px`,
-        });
-        this.textArea.focus && this.textArea.focus();
         this.textArea.one('blur', () => {
             this.destroyTextArea();
             this.isEditing = false;
@@ -412,5 +478,58 @@ Entry.Comment = class Comment {
         delete this.textArea;
 
         Entry.Utils.blur();
+    }
+
+    resizeMouseDown(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (e.button === 0 || (e.originalEvent && e.originalEvent.touches)) {
+            this.setDragInstance(e);
+            this.dragMode = Entry.DRAG_MODE_MOUSEDOWN;
+            this.bindDomEvent(this.resizeMouseMove, this.resizeMouseUp);
+        }
+    }
+
+    resizeMouseMove(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const mouseEvent = Entry.Utils.convertMouseEvent(e);
+        if (
+            this.dragMode === Entry.DRAG_MODE_DRAG ||
+            this.getMouseMoveDiff(mouseEvent) > Entry.BlockView.DRAG_RADIUS
+        ) {
+            if (this.dragMode !== Entry.DRAG_MODE_DRAG) {
+                this.dragMode = Entry.DRAG_MODE_DRAG;
+            }
+            this.resize(
+                (mouseEvent.pageX - this.dragInstance.offsetX) / this.scale,
+                (mouseEvent.pageY - this.dragInstance.offsetY) / this.scale
+            );
+
+            this.dragInstance.set({
+                offsetX: mouseEvent.pageX,
+                offsetY: mouseEvent.pageY,
+            });
+        }
+    }
+
+    resizeMouseUp(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        this.dragMode = Entry.DRAG_MODE_NONE;
+
+        const width = Number(this._comment.getAttribute('width'));
+        const height = Number(this._comment.getAttribute('height'));
+        this.set({
+            width,
+            height,
+        });
+        this.setPosition();
+        this.removeDomEvent();
+        delete this.mouseDownCoordinate;
+        delete this.dragInstance;
     }
 };
