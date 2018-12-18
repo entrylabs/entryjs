15eeb37b14a75b9faacbaacaa1c9c90232ffcc14
youngduk.park <youngduk.park@nhn.com>
2018-12-18 21:34:36 +0900

    147, 함수 블록에 주석 > 해당 함수 블럭을 조립 > 파이썬 모드로 전환 후 다시 블록 모드로 전환 > 함수 블럭 드래그 > 오류 페이지로 이동

--- src/playground/block.js
+++ src/playground/block.js
@@ -345,8 +345,11 @@ Entry.Block = class Block {
         }
 
         if (this._comment) {
-            const comment = this._comment.toJSON();
-            delete comment.id;
+            let comment = this._comment;
+            if (this._comment instanceof Entry.Comment) {
+                comment = this._comment.toJSON();
+                delete comment.id;
+            }
             json.comment = comment;
         }
 
