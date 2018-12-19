b0bac92a589a35cc9a9987753b69c916f4fd34a2
ExtraCold <40051225+extracold1209@users.noreply.github.com>
2018-12-19 14:58:27 +0900

    Update common.js

--- src/util/common.js
+++ src/util/common.js
@@ -4,8 +4,8 @@ const _assign = require('lodash/assign');
 
 exports.callApi = _memoize((key, opt) => {
     let options = _assign({method:"GET"}, opt);
-    if(EntryStatic.api) {
-        options.url = EntryStatic.api + opt.url;
+    if(EntryStatic.baseUrl) {
+        options.url = EntryStatic.baseUrl + opt.url;
     }
     return axios(options);
 });
