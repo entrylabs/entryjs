b51842c71992b2541b8738991c3484aa458b8a05
zwan <zwan@nts-corp.com>
2018-12-26 12:10:47 +0900

    오타

--- src/class/variable_container.js
+++ src/class/variable_container.js
@@ -369,7 +369,7 @@ Entry.VariableContainer = class VariableContainer {
      * @param {object} variable
      */
     renderFunctionReference(func) {
-        const callers = [...this._functionRefs].filter((item) => item.block.data.type === 'func_'+func.Id);
+        const callers = [...this._functionRefs].filter((item) => item.block.data.type === 'func_' + func.id);
 
         func.usedView && $(func.usedView).remove();
         let usedWrapper;
