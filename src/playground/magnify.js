936c2fadd7126cafa809793897b1b24d8cc1b1da
kimorkim <kimorkim@gmail.com>
2019-01-03 19:42:53 +0900

    컬러피커 기초 기능 완료

--- src/playground/magnify.js
+++ src/playground/magnify.js
@@ -1,21 +1,40 @@
 Entry.magnify = class magnify {
     constructor() {
-        const magnify = document.createElement('canvas');
-        magnify.addClass('magnify');
-        magnify.setAttribute('width', '100');
-        magnify.setAttribute('height', '100');
-        var magnifyCtx = magnify.getContext('2d');
-        const main = Entry.stage.canvas.canvas;
-        var ctx = main.getContext('2d');
+        this.mainCanvas = Entry.stage.canvas.canvas;
+        this.color = '';
+        this.renderComponet();
+    }
 
-        const { left = 0, top = 0, width, height } = main.getBoundingClientRect();
-        document.body.appendChild(magnify);
-        document.addEventListener('mousemove', function(e) {
-            var mousePos = {
+    remove() {
+        $(document).off('mousemove.magnify touchmove.magnify');
+        $(document).off('click.magnify');
+        this.$magnify.remove();
+        delete this.color
+    }
+
+    renderComponet() {
+        const magnify = `
+            <canvas width="100px" height="100px" class="magnify"/>
+            <div class="magnifyRect"/>
+        `;
+        const $magnify = $('<div class="entryMagnify">').html(magnify);
+        const magnifyCanvas = $magnify.find('canvas')[0];
+        /** @type {CanvasRenderingContext2D} */
+        const magnifyCtx = magnifyCanvas.getContext('2d');
+        magnifyCtx.fillStyle = 'white';
+        magnifyCtx.imageSmoothingEnabled = false;
+        const { left = 0, top = 0, width, height } = this.mainCanvas.getBoundingClientRect();
+        this.$magnify = $magnify;
+        $('body').append($magnify);
+        $(document).on('mousemove.magnify touchmove.magnify', (e) => {
+            const mousePos = {
                 x: e.clientX,
                 y: e.clientY,
             };
-            var color = undefined;
+
+            let x = Math.floor((mousePos.x - left) * 640 / width) - 17;
+            let y = Math.floor((mousePos.y - top) * 360 / height) - 20;
+            magnifyCtx.drawImage(this.mainCanvas, x, y, 213, 120, 0, 0, 640, 360);
 
             if (
                 mousePos !== null &&
@@ -24,35 +43,19 @@ Entry.magnify = class magnify {
                 mousePos.y > top &&
                 mousePos.y < height + top
             ) {
-                // color picker image is 256x256 and is offset by 10px
-                // from top and bottom
-                var imageData = ctx.getImageData(0, 0, 640, 360);
-                var data = imageData.data;
-
-                let x = Math.floor((mousePos.x - left) * 640 / width);
-                let y = Math.floor((mousePos.y - top) * 360 / height);
-                // let x = mousePos.x - left;
-                // let y = mousePos.y - top;
-                console.log(x, y, mousePos.x - left, mousePos.y - top, left, width);
-                var red = data[(width * y + x) * 4];
-                var green = data[(width * y + x) * 4 + 1];
-                var blue = data[(width * y + x) * 4 + 2];
-                var color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+                $magnify.css('transform', `translate3d(${e.pageX - 50}px, ${e.pageY - 50}px, 0)`);
+                const imageData = magnifyCtx.getImageData(50, 50, 1, 1);
+                const [red, green, blue] = imageData.data;
+                const color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+                if (color) {
+                    magnifyCanvas.style.borderColor = color;
+                    this.color = [red, green, blue];
+                }
             }
-            console.log(color);
-            // magnifyCtx.fillStyle = 'white';
-            // //magnifyCtx.clearRect(0,0, zoom.width, zoom.height);
-            // //magnifyCtx.fillStyle = "transparent";
-
-            // let x = Math.floor((mousePos.x - left) * 640 / width) - 15;
-            // let y = Math.floor((mousePos.y - top) * 360 / height) - 15;
-            // magnifyCtx.drawImage(main, x, y, 213, 120, 0, 0, 640, 360);
-            // magnify.style.top = e.pageY - 50 + 'px';
-            // magnify.style.left = e.pageX - 50 + 'px';
-            // magnify.style.display = 'block';
-            if (color) {
-                magnify.style.borderColor = color;
-            }
+        });
+        $(document).on('click.magnify', () => {
+            console.log(this.color);
+            this.remove();
         });
     }
 };
