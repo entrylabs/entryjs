6271fea1793835747a8403262a176789aa20de59
kimorkim <kimorkim@gmail.com>
2019-01-16 18:01:26 +0900

    이벤트 관리 소스 추가

---
+++ src/util/event.js
@@ -0,0 +1,76 @@
+import _toPairs from 'lodash/toPairs';
+
+class Event {
+    static instance;
+    static elementMap = new Map();
+
+    constructor() {
+        if (Event.instance) {
+            return Event.instance;
+        }
+    }
+
+    on(dom = document, type, callback, option = false) {
+        const eventMap = Event.elementMap.get(dom) || {};
+        if (eventMap[type]) {
+            eventMap[type].push(callback);
+        } else {
+            eventMap[type] = [callback];
+        }
+        Event.elementMap.set(dom, eventMap);
+        this.addEvent(dom, type, callback, option);
+    }
+
+    off(dom = document, type, callback, option = false) {
+        const eventMap = Event.elementMap.get(dom) || {};
+        _toPairs(eventMap).forEach(([key, value = []]) => {
+            const filtered = value.filter((func) => {
+                if (!callback || callback === func) {
+                    if (type === key) {
+                        this.removeEvent(dom, type, func, option);
+                        return false;
+                    } else if (type === undefined) {
+                        this.removeEvent(dom, key, func, option);
+                        return false;
+                    } else if (key.indexOf('.') > -1) {
+                        const [event, namespace = ''] = key.split('.');
+                        const [e, n] = this.getEventName(type);
+                        if (e === event || n === namespace) {
+                            this.removeEvent(dom, key, func, option);
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            });
+            if (filtered.length) {
+                eventMap[key] = filtered;
+            } else {
+                delete eventMap[key];
+            }
+        });
+        Event.elementMap.set(dom, eventMap);
+    }
+
+    getType(type) {
+        return type.split('.')[0];
+    }
+
+    getEventName(type) {
+        if (type.indexOf('.') > -1) {
+            return type.split('.');
+        } else {
+            return [type, type];
+        }
+    }
+
+    addEvent(dom, type, callback, option) {
+        dom.addEventListener(this.getType(type), callback, option);
+    }
+
+    removeEvent(dom, type, callback, option) {
+        dom.removeEventListener(this.getType(type), callback, option);
+    }
+}
+
+export default Event;
