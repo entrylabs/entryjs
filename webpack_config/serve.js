609647b9aa544d74c851f35cda4df1426de19255
kimorkim <kimorkim@gmail.com>
2019-02-13 17:41:57 +0900

    update

--- webpack_config/serve.js
+++ webpack_config/serve.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./common.js');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'development',
@@ -11,61 +13,84 @@ module.exports = merge(common, {
         rules: [
             {
                 test: /\.(css|less)$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                url: false,
-                                minimize: true,
-                                sourceMap: true,
-                            },
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it use publicPath in webpackOptions.output
+                            publicPath: '../',
                         },
-                        {
-                            loader: require.resolve('postcss-loader'),
-                            options: {
-                                ident: 'postcss',
-                                plugins: () => [
-                                    require('postcss-flexbugs-fixes'),
-                                    autoprefixer({
-                                        browsers: [
-                                            '>1%',
-                                            'last 4 versions',
-                                            'Firefox ESR',
-                                            'not ie < 9', // React doesn't support IE8 anyway
-                                        ],
-                                        flexbox: 'no-2009',
-                                        remove: false,
-                                    }),
-                                ],
-                            },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                            minimize: true,
+                            sourceMap: false,
                         },
-                        {
-                            loader: 'less-loader',
-                            options: {
-                                sourceMap: false,
-                            },
+                    },
+                    {
+                        loader: require.resolve('postcss-loader'),
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                require('postcss-flexbugs-fixes'),
+                                autoprefixer({
+                                    browsers: [
+                                        '>1%',
+                                        'last 4 versions',
+                                        'Firefox ESR',
+                                        'not ie < 9', // React doesn't support IE8 anyway
+                                    ],
+                                    flexbox: 'no-2009',
+                                    remove: false,
+                                }),
+                            ],
                         },
-                    ],
-                }),
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            sourceMap: false,
+                        },
+                    },
+                ],
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Entry Example',
+            filename: path.resolve('dist', 'index.html'),
+            template: path.resolve('example', 'example.ejs'),
+            // template: path.resolve('./example/example.html'),
+            inject: false,
+            hash: true,
+        }),
+    ],
     devServer: {
         contentBase: './',
         port: 8080,
-        historyApiFallback: {
-            index: '/example/example.html',
-            rewrites: [
-                { from: /^\/$/, to: '/example/example.html' },
-                { from: /^\/lib\/entry-js/, to: '/' },
-            ],
-        },
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        publicPath: '/',
+        // historyApiFallback: {
+        //     index: '/example/example.html',
+        //     rewrites: [
+        //         { from: /^\/$/, to: '/example/example.html' },
+        //         { from: /^\/lib\/entry-js/, to: '/' },
+        //     ],
+        // },
         proxy: {
             '/lib/entry-js': {
                 target: 'http://localhost:8080',
                 pathRewrite: { '^/lib/entry-js': '' },
+            },
+            '/dist': {
+                target: 'http://localhost:8080',
+                pathRewrite: { '^/dist': '' },
             },
         },
     },
