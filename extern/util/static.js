e2d4771e248799fce812afa55fb63f60f5ad2726
Entry Dev <entrydev@nts-corp.com>
2019-04-26 07:57:25 +0000

    Entry Js PUBLISH by v3.20190426.663

--- extern/util/static.js
+++ extern/util/static.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* eslint-disable */
 var EntryStatic = {};
 
 EntryStatic.objectTypes = ['sprite', 'textBox'];
@@ -686,6 +687,7 @@ EntryStatic.colorSet = {
             VARIABLE: '#CE38CE',
             HARDWARE: '#FFFFFF',
             EXPANSION: '#FF8888',
+            HIDDEN: '#FFFFFF',
         },
     },
     block: {
@@ -703,6 +705,7 @@ EntryStatic.colorSet = {
             FUNC: '#de5c04',
             HARDWARE: '#00b6b1',
             EXPANSION: '#ef6d6d',
+            HIDDEN: '#8aa3b2',
         },
         lighten: {
             START: '#3bce3b',
@@ -718,6 +721,7 @@ EntryStatic.colorSet = {
             FUNC: '#ff7b22',
             HARDWARE: '#78d5d3',
             EXPANSION: '#ffaeae',
+            HIDDEN: '#ffaeae',
         },
         darken: {
             START: '#009400',
@@ -733,6 +737,7 @@ EntryStatic.colorSet = {
             FUNC: '#a14100',
             HARDWARE: '#008380',
             EXPANSION: '#c63f3f',
+            HIDDEN: '#728997',
         },
         emphasize: {
             '#00b400': '#5BC982', //START
