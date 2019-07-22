e48e2954db784cac809a8108dd94dae76b2058de
Entry Dev <entrydev@nts-corp.com>
2019-07-22 02:11:41 +0000

    Entry Js PUBLISH by v3.20190722.794

--- src/playground/blocks/block_expansion_festival.js
+++ src/playground/blocks/block_expansion_festival.js
@@ -7,7 +7,7 @@ function getLocationMap() {
     return {
         Seoul: {
             code: 1,
-                sub: [
+            sub: [
                 '강남구',
                 '강동구',
                 '강북구',
@@ -37,7 +37,7 @@ function getLocationMap() {
         },
         Incheon: {
             code: 2,
-                sub: [
+            sub: [
                 '강화군',
                 '계양구',
                 '남구',
@@ -52,20 +52,20 @@ function getLocationMap() {
         },
         Daejeon: {
             code: 3,
-                sub: ['대덕구', '동구', '서구', '유성구', '중구'],
+            sub: ['대덕구', '동구', '서구', '유성구', '중구'],
         },
         Daegu: {
             code: 4,
-                sub: ['남구', '달서구', '달성군', '동구', '북구', '서구', '수성구', '중구'],
+            sub: ['남구', '달서구', '달성군', '동구', '북구', '서구', '수성구', '중구'],
         },
         Gwangju: {
             code: 5,
-                sub: ['광산구', '남구', '동구', '북구', '서구'],
+            sub: ['광산구', '남구', '동구', '북구', '서구'],
         },
 
         Busan: {
             code: 6,
-                sub: [
+            sub: [
                 '강서구',
                 '금정구',
                 '기장군',
@@ -86,51 +86,51 @@ function getLocationMap() {
         },
         Ulsan: {
             code: 7,
-                sub: ['중구', '남구', '동구', '북구', '울주군'],
+            sub: ['중구', '남구', '동구', '북구', '울주군'],
         },
         Sejong: {
             code: 8,
-                sub: ['세종특별자치시'],
+            sub: ['세종특별자치시'],
         },
         'Gyeonggi-do': {
-        code: 31,
+            code: 31,
             sub: [
-            '가평군',
-            '고양시',
-            '과천시',
-            '광명시',
-            '광주시',
-            '구리시',
-            '군포시',
-            '김포시',
-            '남양주시',
-            '동두천시',
-            '부천시',
-            '성남시',
-            '수원시',
-            '시흥시',
-            '안산시',
-            '안성시',
-            '안양시',
-            '양주시',
-            '양평군',
-            '여주시',
-            '연천군',
-            '오산시',
-            '용인시',
-            '의왕시',
-            '의정부시',
-            '이천시',
-            '파주시',
-            '평택시',
-            '포천시',
-            '하남시',
-            '화성시',
-        ],
-    },
+                '가평군',
+                '고양시',
+                '과천시',
+                '광명시',
+                '광주시',
+                '구리시',
+                '군포시',
+                '김포시',
+                '남양주시',
+                '동두천시',
+                '부천시',
+                '성남시',
+                '수원시',
+                '시흥시',
+                '안산시',
+                '안성시',
+                '안양시',
+                '양주시',
+                '양평군',
+                '여주시',
+                '연천군',
+                '오산시',
+                '용인시',
+                '의왕시',
+                '의정부시',
+                '이천시',
+                '파주시',
+                '평택시',
+                '포천시',
+                '하남시',
+                '화성시',
+            ],
+        },
         Gangwon: {
             code: 32,
-                sub: [
+            sub: [
                 '강릉시',
                 '고성군',
                 '동해시',
@@ -152,147 +152,148 @@ function getLocationMap() {
             ],
         },
         'Chungcheongbuk-do': {
-        code: 33,
+            code: 33,
             sub: [
-            '괴산군',
-            '단양군',
-            '보은군',
-            '영동군',
-            '옥천군',
-            '음성군',
-            '제천시',
-            '진천군',
-            '청원군',
-            '청주시',
-            '충주시',
-            '증평군',
-        ],
-    },
+                '괴산군',
+                '단양군',
+                '보은군',
+                '영동군',
+                '옥천군',
+                '음성군',
+                '제천시',
+                '진천군',
+                '청원군',
+                '청주시',
+                '충주시',
+                '증평군',
+            ],
+        },
         'Chungcheongnam-do': {
-        code: 34,
+            code: 34,
             sub: [
-            '공주시',
-            '금산군',
-            '논산시',
-            '당진시',
-            '보령시',
-            '부여군',
-            '서산시',
-            '서천군',
-            '아산시',
-            '예산군',
-            '천안시',
-            '청양군',
-            '태안군',
-            '홍성군',
-            '계룡시',
-        ],
-    },
+                '공주시',
+                '금산군',
+                '논산시',
+                '당진시',
+                '보령시',
+                '부여군',
+                '서산시',
+                '서천군',
+                '아산시',
+                '예산군',
+                '천안시',
+                '청양군',
+                '태안군',
+                '홍성군',
+                '계룡시',
+            ],
+        },
         'Gyeongsangbuk-do': {
-        code: 35,
+            code: 35,
             sub: [
-            '경산시',
-            '경주시',
-            '고령군',
-            '구미시',
-            '군위군',
-            '김천시',
-            '문경시',
-            '봉화군',
-            '상주시',
-            '성주군',
-            '안동시',
-            '영덕군',
-            '영양군',
-            '영주시',
-            '영천시',
-            '예천군',
-            '울릉군',
-            '울진군',
-            '의성군',
-            '청도군',
-            '청송군',
-            '칠곡군',
-            '포항시',
-        ],
-    },
+                '경산시',
+                '경주시',
+                '고령군',
+                '구미시',
+                '군위군',
+                '김천시',
+                '문경시',
+                '봉화군',
+                '상주시',
+                '성주군',
+                '안동시',
+                '영덕군',
+                '영양군',
+                '영주시',
+                '영천시',
+                '예천군',
+                '울릉군',
+                '울진군',
+                '의성군',
+                '청도군',
+                '청송군',
+                '칠곡군',
+                '포항시',
+            ],
+        },
         'Gyeongsangnam-do': {
-        code: 36,
+            code: 36,
             sub: [
-            '거제시',
-            '거창군',
-            '고성군',
-            '김해시',
-            '남해군',
-            '마산시',
-            '밀양시',
-            '사천시',
-            '산청군',
-            '양산시',
-            '의령군',
-            '진주시',
-            '진해시',
-            '창녕군',
-            '창원시',
-            '통영시',
-            '하동군',
-            '함안군',
-            '함양군',
-            '합천군',
-        ],
-    },
+                '거제시',
+                '거창군',
+                '고성군',
+                '김해시',
+                '남해군',
+                '마산시',
+                '밀양시',
+                '사천시',
+                '산청군',
+                '양산시',
+                '의령군',
+                '진주시',
+                '진해시',
+                '창녕군',
+                '창원시',
+                '통영시',
+                '하동군',
+                '함안군',
+                '함양군',
+                '합천군',
+            ],
+        },
         'Jeollabuk-do': {
-        code: 37,
+            code: 37,
             sub: [
-            '고창군',
-            '군산시',
-            '김제시',
-            '남원시',
-            '무주군',
-            '부안군',
-            '순창군',
-            '완주군',
-            '익산시',
-            '임실군',
-            '장수군',
-            '전주시',
-            '정읍시',
-            '진안군',
-        ],
-    },
+                '고창군',
+                '군산시',
+                '김제시',
+                '남원시',
+                '무주군',
+                '부안군',
+                '순창군',
+                '완주군',
+                '익산시',
+                '임실군',
+                '장수군',
+                '전주시',
+                '정읍시',
+                '진안군',
+            ],
+        },
         'Jeollanam-do': {
-        code: 38,
+            code: 38,
             sub: [
-            '강진군',
-            '고흥군',
-            '곡성군',
-            '광양시',
-            '구례군',
-            '나주시',
-            '담양군',
-            '목포시',
-            '무안군',
-            '보성군',
-            '순천시',
-            '신안군',
-            '여수시',
-            '영광군',
-            '영암군',
-            '완도군',
-            '장성군',
-            '장흥군',
-            '진도군',
-            '함평군',
-            '해남군',
-            '화순군',
-        ],
-    },
+                '강진군',
+                '고흥군',
+                '곡성군',
+                '광양시',
+                '구례군',
+                '나주시',
+                '담양군',
+                '목포시',
+                '무안군',
+                '보성군',
+                '순천시',
+                '신안군',
+                '여수시',
+                '영광군',
+                '영암군',
+                '완도군',
+                '장성군',
+                '장흥군',
+                '진도군',
+                '함평군',
+                '해남군',
+                '화순군',
+            ],
+        },
         Jeju: {
             code: 39,
-                sub: ['남제주군', '북제주군', '서귀포시', '제주시'],
+            sub: ['남제주군', '북제주군', '서귀포시', '제주시'],
         },
     };
 }
+
 Entry.EXPANSION_BLOCK.festival = {
     name: 'festival',
     imageName: 'festival.png',
@@ -301,9 +302,9 @@ Entry.EXPANSION_BLOCK.festival = {
         en: 'festival',
         jp: 'イベント',
     },
-    titleKey: "template.festival_title_text",
+    titleKey: 'template.festival_title_text',
     description: Lang.Msgs.expansion_festival_description,
-    descriptionKey: "Msgs.expansion_festival_description",
+    descriptionKey: 'Msgs.expansion_festival_description',
     isInitialized: false,
     init() {
         if (this.isInitialized) {
@@ -433,7 +434,7 @@ Entry.EXPANSION_BLOCK.festival.getBlocks = function() {
 
     const getDetailInfo = (contentid, defaultValue, infoType) => {
         const key = `festival.api.detail_${contentid}`;
-        return new PromiseManager().Promise(function(resolve) {
+        return new PromiseManager().Promise((resolve) => {
             callApi(key, { url: `${Entry.EXPANSION_BLOCK.festival.api}/${contentid}` })
                 .then((response) => {
                     const item = response.data.response.body.items.item;
@@ -442,9 +443,7 @@ Entry.EXPANSION_BLOCK.festival.getBlocks = function() {
                     }
                     return resolve(defaultValue);
                 })
-                .catch(() => {
-                    return resolve(defaultValue);
-                });
+                .catch(() => resolve(defaultValue));
         });
     };
 
@@ -458,9 +457,7 @@ Entry.EXPANSION_BLOCK.festival.getBlocks = function() {
                     }
                     resolve(defaultValue);
                 })
-                .catch(() => {
-                    return resolve(defaultValue);
-                });
+                .catch(() => resolve(defaultValue));
         });
     };
 
@@ -472,7 +469,7 @@ Entry.EXPANSION_BLOCK.festival.getBlocks = function() {
         const num = number % 10 || 10;
         params.page = Math.floor((number - 1) / 10 + 1);
         const key = `festival.api-${JSON.stringify(params)}`;
-        return new PromiseManager().Promise(function(resolve) {
+        return new PromiseManager().Promise((resolve) => {
             callApi(key, { url: Entry.EXPANSION_BLOCK.festival.api, params })
                 .then((result) => {
                     const items = result.data.response.body.items.item;
@@ -484,9 +481,7 @@ Entry.EXPANSION_BLOCK.festival.getBlocks = function() {
                     }
                     return resolve(item);
                 })
-                .catch(() => {
-                    return resolve(defaultValue);
-                });
+                .catch(() => resolve(defaultValue));
         });
     };
 
@@ -534,8 +529,8 @@ Entry.EXPANSION_BLOCK.festival.getBlocks = function() {
                 const defaultValue = 0;
                 const params = {
                     area:
-                        Entry.EXPANSION_BLOCK.festival.locationMap[
-                            script.getField('LOCATION', script)
+                    Entry.EXPANSION_BLOCK.festival.locationMap[
+                        script.getField('LOCATION', script)
                         ].code,
                     month:
                         Entry.EXPANSION_BLOCK.festival.monthMap[script.getField('MONTH', script)],
