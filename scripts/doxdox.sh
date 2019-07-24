4213239f6bf8cc6015a5172156e03b61ae57e13f
JaewonLee <lee.jaewon@nts-corp.com>
2019-07-24 12:15:24 +0900

    doxdox generating script 작성, gitignore update

--- scripts/doxdox.sh
+++ scripts/doxdox.sh
@@ -1,29 +1,49 @@
-# doxdox 'src/class/container.js' --layout markdown --output documentation/src/class/container.md
-# doxdox 'src/class/dialog.js' --layout markdown --output documentation/src/class/dialog.md
-# doxdox 'src/class/doneProject.js' --layout markdown --output documentation/src/class/doneProject.md
-# doxdox 'src/class/engine.js' --layout markdown --output documentation/src/class/engine.md
-# doxdox 'src/class/entity.js' --layout markdown --output documentation/src/class/entity.md
-# doxdox 'src/class/Expansion.js' --layout markdown --output documentation/src/class/Expansion.md
-# doxdox 'src/class/function.js' --layout markdown --output documentation/src/class/function.md
-# doxdox 'src/class/helper.js' --layout markdown --output documentation/src/class/helper.md
-# doxdox 'src/class/hw_monitor.js' --layout markdown --output documentation/src/class/hw_monitor.md
-# doxdox 'src/class/hw.js' --layout markdown --output documentation/src/class/hw.md
-# doxdox 'src/class/intro.js' --layout markdown --output documentation/src/class/intro.md
-# doxdox 'src/class/moduleManager.js' --layout markdown --output documentation/src/class/moduleManager.md
-# doxdox 'src/class/object.js' --layout markdown --output documentation/src/class/object.md
-# doxdox 'src/class/painter.js' --layout markdown --output documentation/src/class/painter.md
-# doxdox 'src/class/pdf.js' --layout markdown --output documentation/src/class/pdf.md
-# doxdox 'src/class/playground.js' --layout markdown --output documentation/src/class/playground.md
-# doxdox 'src/class/popup_helper.js' --layout markdown --output documentation/src/class/popup_helper.md
-# doxdox 'src/class/popup_list.js' --layout markdown --output documentation/src/class/popup_list.md
-# doxdox 'src/class/popup.js' --layout markdown --output documentation/src/class/popup.md
-# doxdox 'src/class/project.js' --layout markdown --output documentation/src/class/project.md
-# doxdox 'src/class/property_panel.js' --layout markdown --output documentation/src/class/property_panel.md
+mkdir -p documentation/markdown/src/class
+mkdir -p documentation/markdown/src/class/hardware
+mkdir -p documentation/markdown/src/class/maxrect-packer
+mkdir -p documentation/markdown/src/class/pixi/atlas
+mkdir -p documentation/markdown/src/class/pixi/etc
+mkdir -p documentation/markdown/src/class/pixi/handle
+mkdir -p documentation/markdown/src/class/pixi/helper
+mkdir -p documentation/markdown/src/class/pixi/init
+mkdir -p documentation/markdown/src/class/pixi/mesh
+mkdir -p documentation/markdown/src/class/pixi/plugins
+mkdir -p documentation/markdown/src/class/pixi/text
+mkdir -p documentation/markdown/src/class/pixi/utils
+mkdir -p documentation/markdown/src/class/variable
 
-cwd = $pwd
+mkdir -p documentation/bootstrap/src/class
+mkdir -p documentation/bootstrap/src/class/hardware
+mkdir -p documentation/bootstrap/src/class/maxrect-packer
+mkdir -p documentation/bootstrap/src/class/pixi/atlas
+mkdir -p documentation/bootstrap/src/class/pixi/etc
+mkdir -p documentation/bootstrap/src/class/pixi/handle
+mkdir -p documentation/bootstrap/src/class/pixi/helper
+mkdir -p documentation/bootstrap/src/class/pixi/init
+mkdir -p documentation/bootstrap/src/class/pixi/mesh
+mkdir -p documentation/bootstrap/src/class/pixi/plugins
+mkdir -p documentation/bootstrap/src/class/pixi/text
+mkdir -p documentation/bootstrap/src/class/pixi/utils
+mkdir -p documentation/bootstrap/src/class/variable
 
-chmod 777 *
-for file in src/class/*
-do
-    echo "$("$file")"
-done
+for file in $(find src/class/* -name '*.js'); do  
+    doxdox "$file" --layout markdown --output documentation/markdown/"$file".md 
+    doxdox "$file" --layout bootstrap --output documentation/bootstrap/"$file".html
+echo "$file"done; done
+
+# for file in src/class/*
+# do
+#     if [ -d "$file" ]
+#     then
+#         for ff in $file/*
+#         do      
+#             doxdox "$file" --layout markdown --output documentation/markdown/"$file".md
+#             doxdox "$file" --layout bootstrap --output documentation/bootstrap/"$file".html
+#             echo "$file"done
+#         done
+#     else
+#         doxdox "$file" --layout markdown --output documentation/markdown/"$file".md
+#         doxdox "$file" --layout bootstrap --output documentation/bootstrap/"$file".html
+#         echo "$file"done
+#     fi
+# done
