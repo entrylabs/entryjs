a61084b67189e873fbdbd75778d72546f4267f78
extracold1209 <extracold1209@gmail.com>
2019-08-09 17:11:26 +0900

    ISkeleton interface 를 index.d.ts 로 이동 + eslint ts 적용건 수정

--- src/playground/skeleton/index.ts
+++ src/playground/skeleton/index.ts
@@ -1,44 +1,4 @@
-Entry.skeleton = function() {};
-
-type Point = {
-    x: number;
-    y: number;
-};
-
-interface ISkeleton {
-    executable?: boolean;
-    fontSize?: number;
-    movable?: boolean;
-    readOnly?: boolean;
-    nextShadow?: boolean;
-    classes?: string[];
-    color?: string;
-    outerLine?: string;
-    morph?: ['prev', 'next']; // for pebble
-    dropdownHeight?: number; // for pebble
-    path: (blockView: any) => string; // svg path string
-    box: (
-        blockView: any
-    ) => {
-        offsetX: number;
-        offsetY: number;
-        width: number;
-        height: number;
-        marginBottom: number;
-        topFieldHeight?: number;
-    };
-    contentPos: () => Point;
-    magnets?: (
-        blockView: any
-    ) => {
-        next?: Point;
-        previous?: Point;
-        boolean?: {};
-        string?: {};
-        param?: {};
-    };
-    statementPos?: (blockView: any) => Point[];
-}
+Entry.skeleton = {};
 
 // index.js 를 제외한 playground/skeleton 디렉토리 하위의 모든 js 를 불러옴
 // https://webpack.js.org/guides/dependency-management/#requirecontext
