f8b43ded6867e902040961000ffda0538b06529a
lee.hungyu <lee.hungyu@nts-corp.com>
2019-09-18 12:06:50 +0900

    [#10819] instance 를 추가하지 않아 일시정시 시 일시정지 되지 않던 블록에 추가.

--- src/util/utils.js
+++ src/util/utils.js
@@ -2516,6 +2516,7 @@ Entry.Utils.playSound = function(id, option = {}) {
 };
 
 Entry.Utils.addSoundInstances = function(instance) {
+    console.log('add sound instance');
     Entry.soundInstances.push(instance);
     instance.on('complete', () => {
         const index = Entry.soundInstances.indexOf(instance);
