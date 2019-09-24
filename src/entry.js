fd79dff1f48b856f1eef6ba340ec279bd354dae6
박영덕 <prisml@nate.com>
2019-09-24 14:35:04 +0900

    add import LiterallycanvasPainter

--- src/entry.js
+++ src/entry.js
@@ -27,6 +27,7 @@ require('./class/helper');
 require('./class/intro');
 require('./class/object');
 require('./class/painter');
+require('./class/literallycanvasPainter');
 require('./class/pdf');
 require('./class/playground');
 require('./class/popup');
