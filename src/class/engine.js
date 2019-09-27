09e408c035fc1237f821daba41a254158025a9f1
usbsync <usbsync@nts-corp.com>
2019-09-27 18:54:38 +0900

    add turbo mode

--- src/class/engine.js
+++ src/class/engine.js
@@ -398,7 +398,8 @@ Entry.Engine = class Engine {
             return;
         }
         clearInterval(this.ticker);
-        this.ticker = setInterval(this.update, Math.floor(1000 / FPS));
+        Entry.tickTime = Math.floor(1000 / FPS);
+        this.ticker = setInterval(this.update, Entry.tickTime);
         Entry.FPS = FPS;
     }
 
@@ -411,7 +412,8 @@ Entry.Engine = class Engine {
         GEHelper.Ticker.setFPS(Entry.FPS);
 
         if (!this.ticker) {
-            this.ticker = setInterval(this.update, Math.floor(1000 / Entry.FPS));
+            Entry.tickTime = Math.floor(1000 / Entry.FPS);
+            this.ticker = setInterval(this.update, Entry.tickTime);
         }
     }
 
@@ -758,11 +760,6 @@ Entry.Engine = class Engine {
      * @param {string} eventName
      */
     raiseEvent = (entity, eventName) => {
-        console.log(
-            this,
-            entity === this.executeEntity.get(entity),
-            entity.__proto__ === this.executeEntity.get(entity).__proto__
-        );
         entity.parent.script.raiseEvent(eventName, this.executeEntity.get(entity));
     };
 
