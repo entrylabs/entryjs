8855b1bd8b157edc7f4872402a30ff5c2aa92ed4
extracold1209 <extracold1209@gmail.com>
2019-10-15 15:23:57 +0900

    [feature/audio_block] audioInput 의 현재 볼륨을 가져오기 위한 로직 추가

--- src/util/audioUtils.js
+++ src/util/audioUtils.js
@@ -1,4 +1,12 @@
+/**
+ * 언더바가 붙은 함수 및 변수를 외부에서 쓰지 마세요
+ * @author extracold1209
+ */
 class AudioUtils {
+    get currentVolume() {
+        return this._currentVolume;
+    }
+
     constructor() {
         this.isAudioSupport = this._isBrowserSupportAudio(); // 브라우저 지원 확인
         this.isAudioInitComplete = false; // 유저 인풋 연결 확인
@@ -6,10 +14,21 @@ class AudioUtils {
 
         this._userMediaStream = undefined;
         this._mediaRecorder = undefined;
-        this._gainNode = undefined;
+        this._currentVolume = -1;
         this._audioChunks = [];
     }
 
+    /**
+     * 웹 오디오 노드 스트림을 정의한다.
+     * 각 스트림은 아래와 같은 역할을 한다.
+     *
+     * - MediaStreamSource : 오디오 인풋 데이터를 컨트롤 가능한 컨텍스트에 넣기 위한 시점
+     * - GainNode : 전체 볼륨 세기 조정
+     * - AnalyserNode : 현재 버퍼에서 진폭등의 값을 읽어와 표현 가능한 수치로 제공
+     * - ScriptProcessorNode: (deprecated) input - output 사이에서 데이터를 가져와 다양한 로직 추가가능
+     *   여기서는 analyse 된 음세기의 평균값을 도출한 후 그대로 버퍼를 다음으로 전
+     * @returns {Promise<boolean>}
+     */
     async initUserMedia() {
         if (!this.isAudioSupport) {
             console.error('this browser not support input media');
@@ -22,15 +41,17 @@ class AudioUtils {
             });
             const audioContext = new AudioContext();
             const streamSrc = audioContext.createMediaStreamSource(mediaStream);
-            const gainNode = audioContext.createGain();
+            const gainNode = audioContext.createGain(); // 볼륨 전체 세팅용 노드
+            const analyserNode = audioContext.createAnalyser();
+            const scriptNode = audioContext.createScriptProcessor(2048, 1, 1);
             const streamDest = audioContext.createMediaStreamDestination();
             const mediaRecorder = new MediaRecorder(streamDest.stream);
 
-            streamSrc.connect(gainNode);
-            gainNode.connect(streamDest);
+            // 순서대로 노드 커넥션을 맺는다.
+            this._connectNodes(streamSrc, gainNode, analyserNode, scriptNode, streamDest);
+            scriptNode.onaudioprocess = this._handleScriptProcess(analyserNode);
 
             this._userMediaStream = mediaStream;
-            this._gainNode = gainNode;
             this._mediaRecorder = mediaRecorder;
             this.isAudioInitComplete = true;
             return true;
@@ -83,8 +104,14 @@ class AudioUtils {
         return tracks && tracks.some((track) => track.enabled);
     }
 
-    setAudioVolume(volume) {
-
+    _connectNodes(...connectableNodes) {
+        for (let i = 0; i < connectableNodes.length - 1; i++) {
+            if (connectableNodes[i].connect) {
+                connectableNodes[i].connect(connectableNodes[i + 1]);
+            } else {
+                throw new Error('you can not connect node');
+            }
+        }
     }
 
     _isBrowserSupportAudio() {
@@ -101,6 +128,24 @@ class AudioUtils {
 
         const blob = new Blob(this._audioChunks, { type: 'audio/wav' });
         console.log(URL.createObjectURL(blob));
+    };
+
+    _handleScriptProcess = (analyserNode) => (audioProcessingEvent) => {
+        const array = new Uint8Array(analyserNode.frequencyBinCount);
+        analyserNode.getByteFrequencyData(array);
+
+        // 현재 input 의 볼륨세기
+        this._currentVolume = array.reduce((total, data) => total + data, 0) / array.length;
+
+        // 볼륨 변형 없이 그대로 통과
+        const { inputBuffer, outputBuffer } = audioProcessingEvent;
+        for (let channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+            const inputData = inputBuffer.getChannelData(channel);
+            const outputData = outputBuffer.getChannelData(channel);
+            for (let sample = 0; sample < inputBuffer.length; sample++) {
+                outputData[sample] = inputData[sample];
+            }
+        }
     };
 }
 
