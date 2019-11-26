b4b8168689ef534df8105cca37fc3bedaefe09a5
lee.hungyu <lee.hungyu@nts-corp.com>
2019-11-26 19:05:13 +0900

    edit 도중 삭제하는 경우 발생하는 에러 처리.

--- src/class/variable_container.js
+++ src/class/variable_container.js
@@ -1127,6 +1127,7 @@ Entry.VariableContainer = class VariableContainer {
                 e.stopPropagation();
                 entrylms.confirm(Lang.Workspace.will_you_delete_function).then((result) => {
                     if (result === true) {
+                        Entry.do('funcEditEnd', 'cancel');
                         Entry.do('funcRemove', func);
                         this.selected = null;
                     }
