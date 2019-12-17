61243ec3a8e88758740c0c3f7dd82fc4cf8cbd7b
leunge <leunge@naver.com>
2019-12-17 17:00:40 +0900

    !! 제거

--- src/class/variable_container.js
+++ src/class/variable_container.js
@@ -994,7 +994,7 @@ Entry.VariableContainer = class VariableContainer {
 
     isUsedBlockTypeInFunction(blockType) {
         return Object.values(this.functions_).some(
-            (func) => !!func.content.getBlockList(false, blockType).length
+            (func) => func.content.getBlockList(false, blockType).length
         );
     }
 
