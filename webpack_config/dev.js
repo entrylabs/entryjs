c24f89e7a1fb281e0f4961d4183881f99995991c
extracold1209 <extracold1209@gmail.com>
2020-01-13 14:07:12 +0900

    [feature/replace_ts_loader] HardSourceWebpackPlugin dev 환경에서 재적용
    - common 에 있어서 prod 환경에 문제를 주었던 cache 기능을 dev 에만 적용

--- webpack_config/dev.js
+++ webpack_config/dev.js
@@ -1,9 +1,24 @@
 'use strict';
 
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     module: {
         rules: [],
     },
     devtool: 'cheap-source-map',
+    plugins: [
+        new HardSourceWebpackPlugin(),
+        new HardSourceWebpackPlugin.ExcludeModulePlugin([
+            {
+                // HardSource works with mini-css-extract-plugin but due to how
+                // mini-css emits assets, assets are not emitted on repeated builds with
+                // mini-css and hard-source together. Ignoring the mini-css loader
+                // modules, but not the other css loader modules, excludes the modules
+                // that mini-css needs rebuilt to output assets every time.
+                test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
+            },
+        ]),
+    ]
 };
