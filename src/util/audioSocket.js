3c374e97963173b79ac6769ba9b0daf32310892c
chanlee <chanlee@nts-corp.com>
2020-01-20 18:38:36 +0900

    connection error check

--- src/util/audioSocket.js
+++ src/util/audioSocket.js
@@ -7,16 +7,20 @@ const DEFAULT_ADDR = `${window.location.hostname}`;
 
 export function voiceApiConnect(addr = DEFAULT_ADDR, language = 'Kor', cb) {
     return new Promise((resolve, reject) => {
-        const client = io.connect(`https://${addr}`, {
+        const client = io(`https://${addr}`, {
             query: `language=${language}`,
             secure: true,
             reconnect: true,
             rejectUnauthorized: false,
         });
-        client.onerror = function(error) {
-            console.log('Connect Error: ' + JSON.stringify(error));
-        };
-        client.on('open', () => {
+        // client.onerror = function(error) {
+        //     console.log('Connect Error: ' + JSON.stringify(error));
+        // };
+        // client.on('open', () => {
+        //     console.log('NSASR Voice Server Connected');
+        //     resolve(client);
+        // });
+        client.on('connect', () => {
             console.log('NSASR Voice Server Connected');
             resolve(client);
         });
@@ -24,6 +28,19 @@ export function voiceApiConnect(addr = DEFAULT_ADDR, language = 'Kor', cb) {
             Entry.engine.hideAllAudioPanel();
             console.log('closed');
             audioUtils.isRecording = false;
+        });
+
+        socket.on('connect_error', (error) => {
+            console.error('connect_error', error);
+            reject(error);
+        });
+        socket.on('connect_timeout', (timeout) => {
+            console.error('connect_timeout', timeout);
+            reject(timeout);
+        });
+        socket.on('error', (error) => {
+            console.error('error', error);
+            reject(error);
         });
     });
 }
