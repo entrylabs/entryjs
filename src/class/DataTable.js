ef08eeb501d0b27cc7d3c02a4074c5e129a3971a
lee.hungyu <lee.hungyu@nts-corp.com>
2020-01-20 12:09:11 +0900

    테이블 추가하기 로직 구현.

--- src/class/DataTable.js
+++ src/class/DataTable.js
@@ -1,5 +1,6 @@
 import { DataAnalytics } from '@entrylabs/tool';
 import _find from 'lodash/find';
+import _findIndex from 'lodash/findIndex';
 import DataTableSource from './source/DataTableSource';
 
 class DataTable {
@@ -22,17 +23,23 @@ class DataTable {
         return _find(this.#tables, { id });
     }
 
+    getIndex({ id }) {
+        if (!id) {
+            console.warn('empty argument');
+            return null;
+        }
+        return _findIndex(this.#tables, { id });
+    }
+
     addSource(table) {
         let data = table || { name: Lang.Workspace.data_table };
         data.name = Entry.getOrderedName(data.name, this.#tables, 'name');
         const isDataTableSource = data instanceof DataTableSource;
-        this.#tables.unshift(isDataTableSource ? data : new DataTableSource(data));
-        Entry.playground.reloadPlayground();
+        Entry.do('dataTableAddSource', isDataTableSource ? data : new DataTableSource(data));
     }
 
-    removeSource({ id: targetId }) {
-        this.#tables = this.#tables.filter(({ id }) => id === targetId);
-        // Entry.playground.reloadPlayground();
+    removeSource(table) {
+        Entry.do('dataTableRemoveSource', table);
     }
 
     changeItemPosition(start, end) {
