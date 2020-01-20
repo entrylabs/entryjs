f93b36ff0d720e324854959fdd6cc1c20d02cdb9
chanlee <chanlee@nts-corp.com>
2020-01-20 18:52:21 +0900

    handle error

--- src/util/audioSocket.js
+++ src/util/audioSocket.js
@@ -12,6 +12,7 @@ export function voiceApiConnect(addr = DEFAULT_ADDR, language = 'Kor', cb) {
             secure: true,
             reconnect: true,
             rejectUnauthorized: false,
+            timeout: GATEWAY_CONNECT_TIMEOUT,
         });
         // client.onerror = function(error) {
         //     console.log('Connect Error: ' + JSON.stringify(error));
@@ -24,21 +25,27 @@ export function voiceApiConnect(addr = DEFAULT_ADDR, language = 'Kor', cb) {
             console.log('socket connected');
         });
         client.on('disconnect', () => {
-            Entry.engine.hideAllAudioPanel();
             console.log('closed');
+            Entry.engine.hideAllAudioPanel();
             audioUtils.isRecording = false;
         });
 
         client.on('connect_error', (error) => {
             console.error('connect_error', error);
+            Entry.engine.hideAllAudioPanel();
+            audioUtils.isRecording = false;
             reject(error);
         });
         client.on('connect_timeout', (timeout) => {
             console.error('connect_timeout', timeout);
+            Entry.engine.hideAllAudioPanel();
+            audioUtils.isRecording = false;
             reject(timeout);
         });
         client.on('error', (error) => {
             console.error('error', error);
+            Entry.engine.hideAllAudioPanel();
+            audioUtils.isRecording = false;
             reject(error);
         });
     });
