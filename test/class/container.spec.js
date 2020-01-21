5189398489387b950a6f1650a16fdfcaabdc96ad
chanlee <chanlee@nts-corp.com>
2020-01-21 20:47:50 +0900

    touch

--- test/class/container.spec.js
+++ test/class/container.spec.js
@@ -2,4 +2,5 @@ test('Container 샘플 테스트', async () => {
     // global.Entry = {};
     // const Container = await import('../../src/class/container');
     // expect(typeof Entry.Container).toEqual('function');
+    //
 });
