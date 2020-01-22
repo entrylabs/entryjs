7d3f5d12d335027a5cd86e43636b96aa4b06fdf2
Wonchan Lee <wonchan.lee@gmail.com>
2020-01-22 16:24:48 +0900

    touch

--- test/class/container.spec.js
+++ test/class/container.spec.js
@@ -2,5 +2,4 @@ test('Container 샘플 테스트', async () => {
     // global.Entry = {};
     // const Container = await import('../../src/class/container');
     // expect(typeof Entry.Container).toEqual('function');
-    //
 });
