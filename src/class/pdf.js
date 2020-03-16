783fcdc260daf6f793612a63f4a2172f50cc8ac0
extracold1209 <extracold1209@gmail.com>
2020-03-16 18:28:42 +0900

    pdf.js 클래스화 (Entry#lecture 에서 사용되는 클래스)

--- src/class/pdf.js
+++ src/class/pdf.js
@@ -1,35 +1,40 @@
-'use strict';
+/**
+ * 사용처: Entry#lecture 관련 로직 한군데
+ */
 
-Entry.Pdf = function(file) {
-    this.generateView(file);
-};
+class EntryPDF {
+    constructor(file) {
+        this.generateView(file);
+    }
 
-var p = Entry.Pdf.prototype;
+    generateView(file) {
+        const pdfView = Entry.createElement('div', 'entryPdfWorkspace');
+        pdfView.addClass('entryRemove');
+        this._view = pdfView;
 
-p.generateView = function(file) {
-    var pdf = this;
-    var pdfView = Entry.createElement('div', 'entryPdfWorkspace');
-    pdfView.addClass('entryRemove');
-    this._view = pdfView;
+        let url = '/pdfjs/web/viewer.html';
+        if (file && file !== '') {
+            url += `?file=${file}`;
+        }
 
-    var url = '/pdfjs/web/viewer.html';
-    if (file && file != '') url += '?file=' + file;
+        const pdfViewIframe = Entry.createElement('iframe', 'entryPdfIframeWorkspace');
+        pdfViewIframe.setAttribute('id', 'pdfViewIframe');
+        pdfViewIframe.setAttribute('frameborder', 0);
+        pdfViewIframe.setAttribute('src', url);
+        pdfView.appendChild(pdfViewIframe);
+    }
 
-    var pdfViewIframe = Entry.createElement('iframe', 'entryPdfIframeWorkspace');
-    pdfViewIframe.setAttribute('id', 'pdfViewIframe');
-    pdfViewIframe.setAttribute('frameborder', 0);
-    pdfViewIframe.setAttribute('src', url);
-    pdfView.appendChild(pdfViewIframe);
-};
+    getView() {
+        return this._view;
+    }
 
-p.getView = function() {
-    return this._view;
-};
+    resize() {
+        const container = document.getElementById('entryContainerWorkspaceId');
+        const iframe = document.getElementById('pdfViewIframe');
+        const w = container.offsetWidth;
+        iframe.width = `${w}px`;
+        iframe.height = `${(w * 9) / 16}px`;
+    }
+}
 
-p.resize = function() {
-    var container = document.getElementById('entryContainerWorkspaceId');
-    var iframe = document.getElementById('pdfViewIframe');
-    var w = container.offsetWidth;
-    iframe.width = w + 'px';
-    iframe.height = w * 9 / 16 + 'px';
-};
+Entry.Pdf = EntryPDF;
