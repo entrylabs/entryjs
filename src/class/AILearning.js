45d271ef07fb9c60cc1d45e544c8c58b1277fe7e
이훈규 <lee.hungyu@nhn.com>
2020-03-31 11:52:21 +0900

    [#11172] tfjs 제거.

--- src/class/AILearning.js
+++ src/class/AILearning.js
@@ -1,12 +1,7 @@
-import * as tf from '@tensorflow/tfjs';
-
 export default class AILearning {
-    initialized = false;
-    loaded = false;
     #playground;
     #categoryName = 'ai_learning';
     #labels = [];
-    #model;
 
     constructor(playground) {
         this.#playground = playground;
@@ -16,41 +11,12 @@ export default class AILearning {
         return this.#labels;
     }
 
-    async init() {
-        this.initialized = true;
-    }
-
     async load(url, labels = []) {
         this.#labels = labels;
         this.unbanBlocks();
-        await this.init();
-        this.#model = await tf.loadLayersModel(url);
-        this.loaded = true;
         if(this.#playground) {
             this.#playground.reloadPlayground()
         }
-    }
-
-    async predict(canvas) {
-        try {
-            const data = this.preprocessImage(canvas);
-            const result = await this.#model.predict(data);
-            return await this.#namePredictions(result)
-        } catch (e) {
-            console.log(e);
-            return '';
-        }
-
-    }
-
-    #namePredictions = async (logits) => {
-        const values = Array.from(await logits.data());
-        return values
-            .map((probability, index) => ({
-                className: this.#labels[index] || index,
-                probability,
-                index,
-            }));
     }
 
     unbanBlocks() {
@@ -81,23 +47,6 @@ export default class AILearning {
     }
 
     destroy() {
-        this.#model = null;
-        this.#labels = null;
-        this.initialized = false;
-        this.loaded = false;
-    }
-
-    preprocessImage(canvas) {
-        canvas.width = 224;
-        canvas.height = 224;
-        return tf.tidy(() => {
-            const offset = tf.scalar(127.5);
-            return tf.browser
-                .fromPixels(canvas)
-                .toFloat()
-                .sub(offset)
-                .div(offset)
-                .expandDims(0);
-        });
+        this.#labels = [];
     }
 }
