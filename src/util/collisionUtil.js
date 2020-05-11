42b1384420c7a1bd2433abd461a57d8b51646886
JaewonLee_macbookpro <lee.jaewon@nts-corp.com>
2020-05-11 13:05:29 +0900

    ~색이 ~색에 닿았는가 범위 확인 구현

--- src/util/collisionUtil.js
+++ src/util/collisionUtil.js
@@ -3,26 +3,23 @@ import { GEHelper } from '../graphicEngine/GEHelper';
 const ALPHA_THRESHOLD = 0.2;
 const collision = ndgmr.checkPixelCollision;
 
-export const collisionCheckColor = (currentSprite, targetSprite, targetColor1, targetColor2) => {
-    if (targetColor2) {
-        if (
-            checkColor(currentSprite, targetSprite, targetColor2) &&
-            checkColor(targetSprite, currentSprite, targetColor1)
-        ) {
+export const collisionCheckColor = (currentSprite, targetSprite, colorOnTarget, colorOnMe) => {
+    if (colorOnMe) {
+        if (checkColor(currentSprite, targetSprite, colorOnTarget, colorOnMe)) {
             return true;
         } else {
             return false;
         }
     } else {
-        if (checkColor(currentSprite, targetSprite, targetColor1)) {
+        if (checkColor(currentSprite, targetSprite, colorOnTarget)) {
             return true;
         }
     }
 };
 const threshold = 0;
 
-const checkColor = (currentSprite, targetSprite, color) => {
-    const { r, g, b } = color;
+const checkColor = (currentSprite, targetSprite, colorOnTarget, colorOnMe) => {
+    const { r, g, b } = colorOnTarget;
     const currentObject = currentSprite.object;
     const targetObject = targetSprite.object;
     if (targetSprite.type === 'textBox' || currentSprite.type === 'textBox') {
@@ -31,51 +28,31 @@ const checkColor = (currentSprite, targetSprite, color) => {
             targetSprite.getVisible() &&
             collision(currentObject, targetObject, ALPHA_THRESHOLD, false)
         ) {
-            const collisionData = collision(currentObject, targetObject, ALPHA_THRESHOLD, true);
-            const minX = parseInt(collisionData.x + collisionData.width * threshold);
-            const minY = parseInt(collisionData.y + collisionData.height * threshold);
-            const targetWidth = parseInt(collisionData.width * (1 - 2 * threshold));
-            const targetHeight = parseInt(collisionData.height * (1 - 2 * threshold));
-            if (targetWidth < 1 || targetHeight < 1) {
-                return false;
-            }
-            currentSprite.setVisible(false);
-            Entry.stage._app.render();
+            /*
+                @type/collisionInfo { minX, minY, targetWidth, targetHeight }
+            */
+            const collisionInfo = getCollisionAreaInfo(currentObject, targetObject);
 
-            let imageData = null;
-            if (!GEHelper._isWebGL) {
-                // const context = document.getElementById('entryCanvas').getContext('2d');
-                imageData = document
-                    .getElementById('entryCanvas')
-                    .getContext('2d')
-                    .getImageData(minX, minY, targetWidth, targetHeight).data;
-            } else {
-                imageData = new Uint8Array(collisionData.width * collisionData.height * 4);
-                const gl = document.getElementById('entryCanvas').getContext('experimental-webgl', {
-                    preserveDrawingBuffer: true,
-                });
-                gl.readPixels(
-                    minX,
-                    minY,
-                    targetWidth,
-                    targetHeight,
-                    gl.RGBA,
-                    gl.UNSIGNED_BYTE,
-                    imageData
-                );
+            const imageData = getImageDataWithCollisionInfo(currentSprite, collisionInfo);
+            let imageData2 = null;
+
+            if (colorOnTarget) {
+                imageData2 = getImageDataWithCollisionInfo(targetSprite, collisionInfo);
             }
-            currentSprite.setVisible(true);
-            Entry.stage._app.render();
+
             for (let i = 0; i < imageData.length; i += 4) {
-                if (
-                    imageData[i] == r &&
-                    imageData[i + 1] == g &&
-                    imageData[i + 2] == b
-                    // Math.abs(imageData[i + 0] - r) < 3 &&
-                    // Math.abs(imageData[i + 1] - g) < 3 &&
-                    // Math.abs(imageData[i + 2] - b) < 3
-                ) {
-                    return true;
+                if (imageData[i] == r && imageData[i + 1] == g && imageData[i + 2] == b) {
+                    if (!colorOnMe) {
+                        return true;
+                    } else {
+                        if (
+                            imageData2[i] == colorOnMe.r &&
+                            imageData2[i + 1] == colorOnMe.g &&
+                            imageData2[i + 2] == colorOnMe.b
+                        ) {
+                            return true;
+                        }
+                    }
                 }
             }
         }
@@ -92,4 +69,42 @@ const checkColor = (currentSprite, targetSprite, color) => {
             }
         }
     }
+};
+
+const getImageDataWithCollisionInfo = (
+    spriteTobeDiscarded,
+    { minX, minY, targetWidth, targetHeight }
+) => {
+    spriteTobeDiscarded.setVisible(false);
+    Entry.stage._app.render();
+    let imageData = null;
+    if (!GEHelper._isWebGL) {
+        // const context = document.getElementById('entryCanvas').getContext('2d');
+        imageData = document
+            .getElementById('entryCanvas')
+            .getContext('2d')
+            .getImageData(minX, minY, targetWidth, targetHeight).data;
+    } else {
+        imageData = new Uint8Array(collisionData.width * collisionData.height * 4);
+        const gl = document.getElementById('entryCanvas').getContext('experimental-webgl', {
+            preserveDrawingBuffer: true,
+        });
+        gl.readPixels(minX, minY, targetWidth, targetHeight, gl.RGBA, gl.UNSIGNED_BYTE, imageData);
+    }
+    spriteTobeDiscarded.setVisible(true);
+    Entry.stage._app.render();
+    return imageData;
+};
+
+// currentObjects => tobe removed;
+const getCollisionAreaInfo = (currentObject, targetObject) => {
+    const collisionData = collision(currentObject, targetObject, ALPHA_THRESHOLD, true);
+    const minX = parseInt(collisionData.x + collisionData.width * threshold);
+    const minY = parseInt(collisionData.y + collisionData.height * threshold);
+    const targetWidth = parseInt(collisionData.width * (1 - 2 * threshold));
+    const targetHeight = parseInt(collisionData.height * (1 - 2 * threshold));
+    if (targetWidth < 1 || targetHeight < 1) {
+        return {};
+    }
+    return { minX, minY, targetWidth, targetHeight };
 };
