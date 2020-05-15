3e929e22b902b44696c6fac53dba29c5be58fa9c
JaewonLee_macbookpro <lee.jaewon@nts-corp.com>
2020-05-15 12:07:01 +0900

    AIUtilize/Expansion 로직 중복 단일화

--- src/class/AIUtilize.js
+++ src/class/AIUtilize.js
@@ -1,57 +1,22 @@
+import ExtraBlockUtils from '../util/extrablockUtils';
+
 export default class AIUtilize {
     constructor(playground) {
         this.playground = playground;
     }
 
     banAllAIUtilizeBlock() {
-        const { mainWorkspace } = this.playground;
-        if (!mainWorkspace) {
-            return;
-        }
-
-        const blockMenu = _.result(mainWorkspace, 'blockMenu');
-        if (!blockMenu) {
-            return;
-        }
-
-        Object.values(Entry.AI_UTILIZE_BLOCK_LIST).forEach((block) => {
-            blockMenu.banClass(block.name, true);
-            blockMenu.banClass(`${block.name}_legacy`, true);
-        });
+        ExtraBlockUtils.banAllBlocks(this.playground, Entry.AI_UTILIZE_BLOCK_LIST);
     }
 
     banAIUtilizeBlocks(aiUtilizeNames = []) {
-        if (!aiUtilizeNames.length) {
-            return;
-        }
-        const aiUtilizes = Object.keys(Entry.AI_UTILIZE_BLOCK_LIST);
-        const aiUtilizeTypes = aiUtilizeNames.filter((x) => aiUtilizes.includes(x));
-        if (!aiUtilizeTypes.length) {
-            console.warn('not exist aiUtilize', aiUtilizeTypes);
-            return;
-        }
-        const currentObjectId = Entry.playground.object.id;
-        Entry.do('selectObject', currentObjectId);
-        aiUtilizeTypes.forEach((aiUtilize) => {
-            if (this.isActive(aiUtilize)) {
-                const blocks = Entry.AI_UTILIZE_BLOCK_LIST[aiUtilize].getBlocks();
-                Object.keys(blocks).forEach((blockType) => {
-                    Entry.Utils.removeBlockByType(blockType);
-                });
-            }
-        });
-        Entry.do('selectObject', currentObjectId).isPass(true);
-        Entry.do('objectRemoveAIUtilizeBlocks', aiUtilizeTypes).isPass(true);
+        ExtraBlockUtils.banBlocks(aiUtilizeNames, Entry.AI_UTILIZE_BLOCK_LIST, (aiUtilizeTypes) =>
+            Entry.do('objectRemoveAIUtilizeBlocks', aiUtilizeTypes).isPass(true)
+        );
     }
 
     isActive(aiUtilizeName) {
-        const aiUtilize = Entry.AI_UTILIZE_BLOCK_LIST[aiUtilizeName];
-        if (!aiUtilize) {
-            console.warn('not exist aiUtilize', aiUtilize);
-            return;
-        }
-        const blocks = aiUtilize.getBlocks();
-        return Object.keys(blocks).some((blockName) => Entry.Utils.isUsedBlockType(blockName));
+        return ExtraBlockUtils.isActive(aiUtilizeName, Entry.AI_UTILIZE_BLOCK_LIST);
     }
 
     addAIUtilizeBlocks(blockNames) {
@@ -59,17 +24,17 @@ export default class AIUtilize {
     }
 
     getAIUtilizes(blockList) {
-        let aiUtilizeList = [];
-        const aiUtilizeBlockList = Object.keys(Entry.AI_UTILIZE_BLOCK_LIST);
-        blockList.forEach((block) => {
-            const { _schema = {} } = block || {};
-            const { isFor, isNotFor = [] } = _schema;
-            const [aiUtilizeKey] = isNotFor;
-            if (aiUtilizeKey && isFor && isFor.indexOf('category_ai_utilize') >= 0) {
-                aiUtilizeList = _.union(aiUtilizeList, [aiUtilizeKey]);
-            }
-        });
-        return aiUtilizeList;
+        return ExtraBlockUtils.getExtras(blockList, 'category_ai_utilize');
+        // let aiUtilizeList = [];
+        // blockList.forEach((block) => {
+        //     const { _schema = {} } = block || {};
+        //     const { isFor, isNotFor = [] } = _schema;
+        //     const [aiUtilizeKey] = isNotFor;
+        //     if (aiUtilizeKey && isFor && isFor.indexOf('category_ai_utilize') >= 0) {
+        //         aiUtilizeList = _.union(aiUtilizeList, [aiUtilizeKey]);
+        //     }
+        // });
+        // return aiUtilizeList;
     }
 
     destroy() {
