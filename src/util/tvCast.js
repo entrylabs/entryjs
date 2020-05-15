84bf12e1ac648f32ffef724065fb9a3cdaba8c27
JaewonLee_macbookpro <lee.jaewon@nts-corp.com>
2020-05-15 14:11:26 +0900

    tvCast prototype-> class

--- src/util/tvCast.js
+++ src/util/tvCast.js
@@ -1,42 +1,37 @@
-'use strict';
+Entry.TvCast = class TvCast {
+    constructor(tvCast) {
+        this.generateView(tvCast);
+    }
+    init(tvCastHash) {
+        this.tvCastHash = tvCastHash;
+    }
 
-Entry.TvCast = function(tvCast) {
-    this.generateView(tvCast);
-};
+    generateView(tvCastHash) {
+        var movieContainer = Entry.createElement('div');
+        movieContainer.addClass('entryContainerMovieWorkspace');
+        movieContainer.addClass('entryRemove');
 
-var p = Entry.TvCast.prototype;
+        this.movieContainer = movieContainer;
 
-p.init = function(tvCastHash) {
-    this.tvCastHash = tvCastHash;
-};
+        var movieContainer = this.movieContainer;
+        const iframe = Entry.createElement('iframe');
+        iframe.setAttribute('id', 'tvCastIframe');
+        iframe.setAttribute('allowfullscreen', '');
+        iframe.setAttribute('frameborder', 0);
+        iframe.setAttribute('src', tvCastHash);
+        this.movieFrame = iframe;
 
-p.generateView = function(tvCastHash) {
-    var movieContainer = Entry.createElement('div');
-    movieContainer.addClass('entryContainerMovieWorkspace');
-    movieContainer.addClass('entryRemove');
+        this.movieContainer.appendChild(this.movieFrame);
+    }
 
-    this.movieContainer = movieContainer;
-    var view = this.movieContainer;
+    getView() {
+        return this.movieContainer;
+    }
 
-    var movieContainer = this.movieContainer;
-    var iframe = Entry.createElement('iframe');
-    iframe.setAttribute('id', 'tvCastIframe');
-    iframe.setAttribute('allowfullscreen', '');
-    iframe.setAttribute('frameborder', 0);
-    iframe.setAttribute('src', tvCastHash);
-    this.movieFrame = iframe;
-
-    this.movieContainer.appendChild(this.movieFrame);
-};
-
-p.getView = function() {
-    return this.movieContainer;
-};
-
-p.resize = function() {
-    var container = document.getElementById('entryContainerWorkspaceId');
-    var iframe = document.getElementById('tvCastIframe');
-    var w = this.movieContainer.offsetWidth;
-    iframe.width = w + 'px';
-    iframe.height = w * 9 / 16 + 'px';
+    resize() {
+        const iframe = document.getElementById('tvCastIframe');
+        const w = this.movieContainer.offsetWidth;
+        iframe.width = `${w}px`;
+        iframe.height = `${(w * 9) / 16}px`;
+    }
 };
