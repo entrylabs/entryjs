659e2352e7206dbde60cccad05cc01b98233b9f3
tyranno <tyranno1223@gmail.com>
2021-01-03 23:55:43 +0900

    offile build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -e # 에러 발생 시 스크립트 중단
 
 export NODE_ENV=production
-git clone -b build "${GH_REPO}" build
+#git clone -b build "${GH_REPO}" build
 rm -rf build/**/* || exit 0
 npm run dist
 cp -r dist build/
@@ -12,5 +12,6 @@ cp -r weights build/
 rsync -R src/playground/block_entry.js build
 rsync -R src/playground/block_entry_mini.js build
 rsync -r -R src/playground/blocks/ build
+rsync -rR package.json build
 ls -al
 ls -al build
