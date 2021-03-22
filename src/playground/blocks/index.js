298f1c34f5389d80d3b8f59b1322e270624faa07
Entry Dev <entrydev@nts-corp.com>
2021-03-22 10:25:25 +0000

    Entry Js deploy dist/v1.9.16

--- src/playground/blocks/index.js
+++ src/playground/blocks/index.js
@@ -49,12 +49,8 @@ Entry.EXPANSION_BLOCK_LIST = {
 function getBlockObject(items) {
     const blockObject = {};
     items.forEach((item) => {
-        try {
-            if ('getBlocks' in item) {
-                Object.assign(blockObject, item.getBlocks());
-            }
-        } catch (err) {
-            console.log(err, item);
+        if ('getBlocks' in item) {
+            Object.assign(blockObject, item.getBlocks());
         }
     });
     return blockObject;
