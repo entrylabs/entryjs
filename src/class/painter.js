8ca4135ffdd9d3c049654503de57b17f0e22cab5
박영덕[SW교육플랫폼개발] <youngduk.park@nts-corp.com>
2022-05-31 18:29:47 +0900

    [painter] 벡터그림판에서 빈 이미지 저장시 방어코드 추가

--- src/class/painter.js
+++ src/class/painter.js
@@ -1,5 +1,9 @@
 import Extension from '../extensions/extension';
 
+// 1px * 1px jpeg iamge
+const NULL_IMAGE =
+    // eslint-disable-next-line max-len
+    'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAIBAQIBAQICAgICAgICAwUDAwMDAwYEBAMFBwYHBwcGBwcICQsJCAgKCAcHCg0KCgsMDAwMBwkODw0MDgsMDAz/2wBDAQICAgMDAwYDAwYMCAcIDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAz/wAARCAABAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD9/KKKKAP/2Q==';
 let EntryPaint;
 Entry.Painter = class Painter {
     constructor(view) {
@@ -219,7 +223,7 @@ Entry.Painter = class Painter {
         if (!Entry.stage.selectedObject) {
             return;
         }
-        const dataURL = this.entryPaint.getDataURL();
+        const dataURL = this.entryPaint.getDataURL() || NULL_IMAGE;
         if (this.entryPaint.mode === this.graphicsMode.VECTOR) {
             this.file.svg = this.entryPaint.exportSVG();
         } else {
