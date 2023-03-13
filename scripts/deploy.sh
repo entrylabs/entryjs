7f267af686ace52456bfd025e44fff715b864165
JY Kim <kimorkim@gmail.com>
2023-03-13 14:49:27 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,10 @@ set -e # 에러 발생 시 스크립트 중단
 git config --local user.email "entrydev@nts-corp.com"
 git config --local user.name "Entry Dev"
 
+pwd
+ls
+ls ..
+
 rm .gitignore
 branchName=${GITHUB_REF##*/}
 deployName="dist/$branchName"
