05dc798c6a762999013ac39eb13423bfe76acd81
Tnks2U <Tnks2UWork@gmail.com>
2023-04-20 18:23:41 +0900

    refactor: 코알라보드 lint 적용

--- src/playground/blocks/hardware/block_coalaboard.js
+++ src/playground/blocks/hardware/block_coalaboard.js
@@ -27,9 +27,9 @@ Entry.Coalaboard = {
         LEDB: 16,
     },
     INEQ_SIGN: [
-        ["<", "<"],
-        [">", ">"],
-        ["=", "="]
+        ['<', '<'],
+        ['>', '>'],
+        ['=', '='],
     ],
     sensorList: function() {
         var list = [];
@@ -80,12 +80,13 @@ Entry.Coalaboard = {
     setZero: function() {
         let sq = Entry.hw.sendQueue;
         for (let port in Entry.Coalaboard.PORT_MAP) {
-            let portData    = Entry.hw.portData[port];
-            if( portData != null ) {
-                if( portData.type == Entry.Coalaboard.SENSOR_MAP[18] ) {     // DC모터 인 경우, 129로 세팅하여 바로 멈추기    
+            let portData = Entry.hw.portData[port];
+            if (portData != null) {
+                if (portData.type == Entry.Coalaboard.SENSOR_MAP[18]) {
+                    // DC모터 인 경우, 129로 세팅하여 바로 멈추기
                     sq[port] = 129;
                 } else {
-                    sq[port] = 0;    
+                    sq[port] = 0;
                 }
             } else {
                 sq[port] = 0;
@@ -107,35 +108,36 @@ Entry.Coalaboard = {
     dcMinValue: -100,
     /**
      * 콜백 함수. 계속해서 센서 데이터를 받는다.
-     * @param {*} pd 
+     * @param {*} pd
      */
     afterReceive(pd) {
-        for( let i = 1; i < 5; i++ ) {      // 오직 센서만 받기
-            let obj = pd[ i ];              // ex) null or { type: "touch", value: 1023 }
-            if( obj != null ) {
-                if( obj.type == 'touch' && obj.value == 0 ) {
+        for (let i = 1; i < 5; i++) {
+            // 오직 센서만 받기
+            let obj = pd[i]; // ex) null or { type: "touch", value: 1023 }
+            if (obj != null) {
+                if (obj.type == 'touch' && obj.value == 0) {
                     Entry.engine.fireEvent('coalaboardButtonEventReceive');
                 }
                 Entry.engine.fireEvent('coalaboardSensorGetValueEventReceive');
             }
         }
     },
-    calculateDCMotorValue: function( value ) {
+    calculateDCMotorValue: function(value) {
         let val = 0;
-        if ( value > 0 ) { 
-            val  = Math.floor( ( value * 0.8 ) + 16 );
-        } else if ( value < 0 ) {
-            val  = Math.ceil( ( value * 0.8 ) - 19 );
-        } else { 
-            val  = 0; 
+        if (value > 0) {
+            val = Math.floor(value * 0.8 + 16);
+        } else if (value < 0) {
+            val = Math.ceil(value * 0.8 - 19);
+        } else {
+            val = 0;
         }
         // DC_MOTOR_ADJUSTMENT  128
         val = 128 + val;
-        if ( val == 128 ) {
+        if (val == 128) {
             val = 129;
         }
         return val;
-    }
+    },
 };
 
 Entry.Coalaboard.monitorTemplate = function() {
@@ -145,45 +147,45 @@ Entry.Coalaboard.monitorTemplate = function() {
         width: 400,
         height: 400,
         listPorts: {
-            'UserInput': {
+            UserInput: {
                 name: Lang.Blocks.COALABOARD_UserInput,
                 type: 'input',
                 pos: { x: 0, y: 0 },
             },
-            'potentiometer': {
+            potentiometer: {
                 name: Lang.Blocks.COALABOARD_potentiometer,
                 type: 'input',
                 pos: { x: 0, y: 0 },
             },
-            'MIC': {
+            MIC: {
                 name: Lang.Blocks.COALABOARD_MIC,
                 type: 'input',
                 pos: { x: 0, y: 0 },
             },
-            'IR': {
+            IR: {
                 name: Lang.Blocks.COALABOARD_IR,
                 type: 'input',
                 pos: { x: 0, y: 0 },
             },
-            'temperature': {
+            temperature: {
                 name: Lang.Blocks.COALABOARD_temperature,
                 type: 'input',
                 pos: { x: 0, y: 0 },
             },
-            'light': {
+            light: {
                 name: Lang.Blocks.COALABOARD_light,
                 type: 'input',
                 pos: { x: 0, y: 0 },
             },
-            'touch': {
+            touch: {
                 name: Lang.Blocks.COALABOARD_touch,
                 type: 'input',
                 pos: { x: 0, y: 0 },
             },
         },
         mode: 'both',
-    }
-}
+    };
+};
 
 Entry.Coalaboard.blockMenuBlocks = [
     'coalaboard_when_button_pressed',
@@ -196,7 +198,7 @@ Entry.Coalaboard.blockMenuBlocks = [
     'coalaboard_turn_on_color_led_by_picker',
     'coalaboard_turn_on_color_led_by_value',
     'coalaboard_turn_off_color_led',
-    'coalaboard_buzzer',    
+    'coalaboard_buzzer',
     'coalaboard_servomotor_angle',
     'coalaboard_dc_direction_speed',
     'coalaboard_dc_speed',
@@ -204,9 +206,8 @@ Entry.Coalaboard.blockMenuBlocks = [
 ];
 
 Entry.Coalaboard.getBlocks = function() {
-    let options_COALABOARD_button2    = 
-    [
-        [Lang.Blocks.COALABOARD_button_pressed,  'pressed'],
+    let options_COALABOARD_button2 = [
+        [Lang.Blocks.COALABOARD_button_pressed, 'pressed'],
         [Lang.Blocks.COALABOARD_button_released, 'released'],
     ];
     return {
@@ -224,9 +225,9 @@ Entry.Coalaboard.getBlocks = function() {
                     size: 12,
                     position: {
                         x: 0,
-                        y: 0
-                    }
-                },                                
+                        y: 0,
+                    },
+                },
                 {
                     type: 'DropdownDynamic',
                     value: null,
@@ -243,17 +244,17 @@ Entry.Coalaboard.getBlocks = function() {
             },
             paramsKeyMap: {
                 DUMMY: 0,
-                PORT: 1
+                PORT: 1,
             },
             class: 'event',
             isNotFor: ['coalaboard'],
             event: 'coalaboardButtonEventReceive',
             func: function(sprite, script) {
-                let selectedSensor  = script.values[ 1 ];
+                let selectedSensor = script.values[1];
                 let port = script.getStringField('PORT');
                 let type = Entry.hw.portData[port].type;
-                let val  = Entry.hw.portData[port].value;       // 0이면 누름, 1023이면 누르지 않음
-                if( selectedSensor == port ) {
+                let val = Entry.hw.portData[port].value; // 0이면 누름, 1023이면 누르지 않음
+                if (selectedSensor == port) {
                     if (val == 0) {
                         return script.callReturn();
                     } else {
@@ -264,7 +265,7 @@ Entry.Coalaboard.getBlocks = function() {
                 }
             },
             syntax: { js: [], py: ['Coalaboard.when_button_pressed(%2, %3)'] },
-        },        
+        },
         coalaboard_when_sensor_get_value: {
             color: EntryStatic.colorSet.block.default.HARDWARE,
             outerLine: EntryStatic.colorSet.block.darken.HARDWARE,
@@ -278,9 +279,9 @@ Entry.Coalaboard.getBlocks = function() {
                     size: 12,
                     position: {
                         x: 0,
-                        y: 0
-                    }
-                },                
+                        y: 0,
+                    },
+                },
                 {
                     type: 'DropdownDynamic',
                     value: null,
@@ -311,7 +312,7 @@ Entry.Coalaboard.getBlocks = function() {
                     {
                         type: 'text',
                         params: ['100'],
-                    }
+                    },
                 ],
                 type: 'coalaboard_when_sensor_get_value',
             },
@@ -319,29 +320,29 @@ Entry.Coalaboard.getBlocks = function() {
                 DUMMY: 0,
                 PORT: 1,
                 INEQ_SIGN: 2,
-                VALUE: 3
+                VALUE: 3,
             },
             class: 'event',
             isNotFor: ['coalaboard'],
             event: 'coalaboardSensorGetValueEventReceive',
             func: function(sprite, script) {
-                let selectedPort    = script.values[ 1 ];
-                let ineqSign        = script.values[ 2 ];
-                let value           = script.values[ 3 ];
-                let port    = script.getStringField('PORT');
-                let val     = Entry.hw.portData[port].value;
-                if( selectedPort == port && ineqSign == '<' && val < value ) {
+                let selectedPort = script.values[1];
+                let ineqSign = script.values[2];
+                let value = script.values[3];
+                let port = script.getStringField('PORT');
+                let val = Entry.hw.portData[port].value;
+                if (selectedPort == port && ineqSign == '<' && val < value) {
                     return script.callReturn();
-                } else if( selectedPort == port && ineqSign == '>' && val > value ) {
+                } else if (selectedPort == port && ineqSign == '>' && val > value) {
                     return script.callReturn();
-                } else if( selectedPort == port && ineqSign == '=' && val == value ) {
+                } else if (selectedPort == port && ineqSign == '=' && val == value) {
                     return script.callReturn();
                 } else {
                     return this.die();
                 }
             },
             syntax: { js: [], py: ['Coalaboard.when_sensor_get_value(%2,%3,%4)'] },
-        },        
+        },
         coalaboard_sensor_value: {
             color: EntryStatic.colorSet.block.default.HARDWARE,
             outerLine: EntryStatic.colorSet.block.darken.HARDWARE,
@@ -487,7 +488,7 @@ Entry.Coalaboard.getBlocks = function() {
                     fontSize: 11,
                     bgColor: EntryStatic.colorSet.block.darken.HARDWARE,
                     arrowColor: EntryStatic.colorSet.arrow.default.HARDWARE,
-                },                
+                },
             ],
             events: {},
             def: {
@@ -496,21 +497,21 @@ Entry.Coalaboard.getBlocks = function() {
             },
             paramsKeyMap: {
                 PORT: 0,
-                PRESSED: 1                
+                PRESSED: 1,
             },
             class: 'button',
             isNotFor: ['coalaboard'],
             func: function(sprite, script) {
                 let port = script.getStringField('PORT');
-                let val  = Entry.hw.portData[port].value;
+                let val = Entry.hw.portData[port].value;
                 let pressed = script.getStringField('PRESSED');
-                if ((pressed == 'pressed') && (val == 0)) {
+                if (pressed == 'pressed' && val == 0) {
                     return true;
-                } else if ((pressed == 'released') && (val == 1023)) {
+                } else if (pressed == 'released' && val == 1023) {
                     return true;
                 } else {
                     return false;
-                }     
+                }
             },
             syntax: { js: [], py: ['Coalaboard.is_touch_pressed(%1, %2)'] },
         },
@@ -550,35 +551,35 @@ Entry.Coalaboard.getBlocks = function() {
                     {
                         type: 'text',
                         params: ['100'],
-                    }
+                    },
                 ],
                 type: 'coalaboard_is_sensor_value_compare',
             },
             paramsKeyMap: {
                 PORT: 0,
                 INEQ_SIGN: 1,
-                VALUE: 2
+                VALUE: 2,
             },
             class: 'button',
             isNotFor: ['coalaboard'],
             func: function(sprite, script) {
-                let selectedPort    = script.values[ 0 ];
-                let ineqSign        = script.values[ 1 ];
-                let value           = script.values[ 2 ];
-                let port    = script.getStringField('PORT');
-                let val     = Entry.hw.portData[port].value;
-                if( selectedPort == port && ineqSign == '<' && val < value ) {
+                let selectedPort = script.values[0];
+                let ineqSign = script.values[1];
+                let value = script.values[2];
+                let port = script.getStringField('PORT');
+                let val = Entry.hw.portData[port].value;
+                if (selectedPort == port && ineqSign == '<' && val < value) {
                     return true;
-                } else if( selectedPort == port && ineqSign == '>' && val > value ) {
+                } else if (selectedPort == port && ineqSign == '>' && val > value) {
                     return true;
-                } else if( selectedPort == port && ineqSign == '=' && val == value ) {
+                } else if (selectedPort == port && ineqSign == '=' && val == value) {
                     return true;
                 } else {
                     return false;
                 }
             },
             syntax: { js: [], py: ['Bitbrick.is_sensor_value_compare(%1,%2,%3)'] },
-        },        
+        },
         coalaboard_turn_off_color_led: {
             color: EntryStatic.colorSet.block.default.HARDWARE,
             outerLine: EntryStatic.colorSet.block.darken.HARDWARE,
@@ -889,7 +890,7 @@ Entry.Coalaboard.getBlocks = function() {
                 var value = script.getNumberValue('VALUE');
                 value = Math.min(value, Entry.Coalaboard.dcMaxValue);
                 value = Math.max(value, Entry.Coalaboard.dcMinValue);
-                let val = Entry.Coalaboard.calculateDCMotorValue( value );
+                let val = Entry.Coalaboard.calculateDCMotorValue(value);
                 Entry.hw.sendQueue[script.getStringField('PORT')] = val;
                 return script.callReturn();
             },
@@ -952,13 +953,13 @@ Entry.Coalaboard.getBlocks = function() {
             isNotFor: ['coalaboard'],
             func: function(sprite, script) {
                 let isFront = script.getStringField('DIRECTION') === 'CW';
-                let value   = script.getNumberValue('VALUE');
+                let value = script.getNumberValue('VALUE');
                 value = Math.min(value, Entry.Coalaboard.dcMaxValue);
                 value = Math.max(value, 0);
-                if ( !isFront ) {
+                if (!isFront) {
                     value = -1 * value;
                 }
-                let val = Entry.Coalaboard.calculateDCMotorValue( value );
+                let val = Entry.Coalaboard.calculateDCMotorValue(value);
                 Entry.hw.sendQueue[script.getStringField('PORT')] = val;
                 return script.callReturn();
             },
@@ -1030,7 +1031,8 @@ Entry.Coalaboard.setLanguage = function() {
                 coalaboard_is_sensor_value_compare: '%1 값 %2 %3 인가?',
                 coalaboard_sensor_value: '%1 값',
                 coalaboard_convert_scale: '변환 %1 값 %2 ~ %3 에서 %4 ~ %5',
-                coalaboard_turn_on_color_led_by_rgb: '엘이디를 빨강 %1 초록 %2 파랑 %3 %4 (으)로 켜기',
+                coalaboard_turn_on_color_led_by_rgb:
+                    '엘이디를 빨강 %1 초록 %2 파랑 %3 %4 (으)로 켜기',
                 coalaboard_turn_on_color_led_by_picker: '엘이디를 %1 (으)로 켜기 %2',
                 coalaboard_turn_on_color_led_by_value: '엘이디를 %1 (으)로 켜기 %2',
                 coalaboard_turn_off_color_led: '엘이디 끄기 %1',
@@ -1042,7 +1044,7 @@ Entry.Coalaboard.setLanguage = function() {
             },
             Blocks: {
                 COALABOARD_button_pressed: '누름',
-                COALABOARD_button_released: '누르지 않음',                
+                COALABOARD_button_released: '누르지 않음',
                 COALABOARD_light: '밝기 센서',
                 COALABOARD_IR: '적외선 센서',
                 COALABOARD_touch: '버튼',
@@ -1063,17 +1065,20 @@ Entry.Coalaboard.setLanguage = function() {
                 coalaboard: '코알라보드',
             },
             Helper: {
-                coalaboard_when_button_pressed: '비트브릭 버튼을 누르면 아래에 연결된 블록들을 실행합니다.',
+                coalaboard_when_button_pressed:
+                    '비트브릭 버튼을 누르면 아래에 연결된 블록들을 실행합니다.',
                 coalaboard_when_sensor_get_value:
                     '비트브릭 센서의 값과 오른쪽에 입력한 값을 비교합니다.\n< : 센서 값이 오른쪽에 위치한 값보다 작은 경우 ‘참’으로 판단합니다.\n> : 센서 값이 오른쪽에 위치한 값보다 큰 경우 ‘참으로 판단합니다.\n= :  센서 값이 오른쪽에 위치한 값과 같은 경우 ‘참으로 판단합니다.',
                 coalaboard_sensor_value:
                     '비트브릭 센서를 사용할 수 있는 블록입니다. 센서값의 범위는 0 ~1023입니다. 메인보드에 연결된 센서의 종류와 포트번호를 자동으로 인식합니다. 블록 안의 화살표를 눌러 사용하려고 하는 센서를 선택하세요.',
                 coalaboard_convert_scale: '비트브릭 센서의 값의 범위를 바꿀 수 있습니다.',
-                coalaboard_is_touch_pressed: '비트브릭 센서 중 버튼을 눌렀을 경우 ‘참’으로 판단합니다.',
+                coalaboard_is_touch_pressed:
+                    '비트브릭 센서 중 버튼을 눌렀을 경우 ‘참’으로 판단합니다.',
                 coalaboard_turn_off_color_led: '비트브릭 엘이디를 끕니다.',
                 coalaboard_turn_on_color_led_by_rgb:
                     '비트브릭 엘이디를 빛의 삼원색인 빨강,초록,파랑을 혼합하여 켭니다. 값의 범위는 0 ~ 255입니다.',
-                coalaboard_turn_on_color_led_by_picker: '비트브릭 엘이디를 색상 창을 사용해 켭니다.',
+                coalaboard_turn_on_color_led_by_picker:
+                    '비트브릭 엘이디를 색상 창을 사용해 켭니다.',
                 coalaboard_turn_on_color_led_by_value:
                     '비트브릭 엘이디를 색상 값으로 켭니다. 값의 범위는 0 ~ 199입니다.',
                 coalaboard_buzzer:
@@ -1084,8 +1089,8 @@ Entry.Coalaboard.setLanguage = function() {
                 coalaboard_dc_direction_speed:
                     '비트브릭 디씨모터의 방향과 속력을 제어합니다. 방향은 시계방향과 반시계방향을 선택할 수 있습니다. 속력 값의 범위는 0 ~ 100입니다. 속력이 0일 때는 회전을 멈춥니다.',
                 coalaboard_servomotor_angle:
-                    '비트브릭 서보모터의 각도를 제어합니다. 각도 값의 범위는 0 ~ 180입니다.'
-            }
+                    '비트브릭 서보모터의 각도를 제어합니다. 각도 값의 범위는 0 ~ 180입니다.',
+            },
         },
         en: {
             // en.js에 작성하던 내용
@@ -1108,7 +1113,7 @@ Entry.Coalaboard.setLanguage = function() {
             },
             Blocks: {
                 COALABOARD_button_pressed: 'pressed',
-                COALABOARD_button_released: 'released',                       
+                COALABOARD_button_released: 'released',
                 COALABOARD_light: 'light',
                 COALABOARD_IR: 'IR',
                 COALABOARD_touch: 'touch',
