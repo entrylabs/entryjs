c6723c1afd6ec635f329485c6d3e3d49eb6cca10
JY Kim <kimorkim@gmail.com>
2023-07-20 11:20:31 +0900

    Update src/class/pixi/etc/PIXIBrushAdaptor.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/class/pixi/etc/PIXIBrushAdaptor.ts
+++ src/class/pixi/etc/PIXIBrushAdaptor.ts
@@ -57,7 +57,7 @@ export class PIXIBrushAdaptor {
 
     endFill() {
         // #10141 때문에 closePath 사용안함.
-        if (!this._shape || this._shape.destroyed) return;
+        if (!this._shape || this._shape.destroyed) {return;}
         this._shape.closePath();
     }
 
