dd3f39c355a4ea9bccc0a3d4ee224544eedce45e
Youngduk Park <0duck@kakao.com>
2023-07-20 11:25:53 +0900

    Update src/class/pixi/etc/PIXIBrushAdaptor.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/class/pixi/etc/PIXIBrushAdaptor.ts
+++ src/class/pixi/etc/PIXIBrushAdaptor.ts
@@ -57,7 +57,9 @@ export class PIXIBrushAdaptor {
 
     endFill() {
         // #10141 때문에 closePath 사용안함.
-        if (!this._shape || this._shape.destroyed) {return;}
+        if (!this._shape || this._shape.destroyed) {
+            return;
+        }
         this._shape.closePath();
     }
 
