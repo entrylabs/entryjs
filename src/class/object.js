4b3ce5c1bec1bb994b34b607470cbab54f4a9f07
usbsync <usbsync@nts-corp.com>
2023-08-29 11:28:31 +0900

    복제본 앞뒤 순서 보정

--- src/class/object.js
+++ src/class/object.js
@@ -544,9 +544,9 @@ Entry.EntryObject = class {
         this.clonedEntities.push(clonedEntity);
         let targetIndex = Entry.stage.selectedObjectContainer.getChildIndex(entity.object);
 
-        const offsetCount =
-            1 + (entity.shapes.length ? 1 : 0) + (entity.paintShapes.length ? 1 : 0);
+        const offsetCount = (entity.shapes.length ? 1 : 0) + (entity.paintShapes.length ? 1 : 0);
         targetIndex -= offsetCount + entity.stamps.length;
+
         Entry.stage.loadEntity(clonedEntity, targetIndex);
 
         if (entity.brush) {
