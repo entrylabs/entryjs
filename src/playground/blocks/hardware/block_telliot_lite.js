3cbe40289431d2069a7b8be3a092679049b149c1
CHOHYEONUNG <macusers2@naver.com>
2024-07-05 16:07:33 +0900

    금쪽이 카 이름 수정 및 isNotFor 작성

--- src/playground/blocks/hardware/block_telliot_lite.js
+++ src/playground/blocks/hardware/block_telliot_lite.js
@@ -5,13 +5,13 @@ class TelliotLite extends TelliotBase {
 	constructor() {
 		super();
 
-		this.id =  'FF.FF';
-		this.name = 'TelliotLite';
-		this.url = 'http://www.telliot.co.kr';
+		this.id =  '60.1';
+		this.name = 'GoldenKidsAICar';
+		this.url = 'https://www.iotedcorp.com';
 		this.imageName = 'telliot_lite.png';
 		this.title = {
-			ko: '텔리엇 라이트',
-			en: 'Telliot Lite',
+			ko: '금쪽이 카',
+			en: 'GoldenKidsAICar',
 		};
 		this.blockMenuBlocks = this.getBlockMenuBlocks();
 	}
@@ -49,10 +49,10 @@ class TelliotLite extends TelliotBase {
 				template: {
 					// Telliot Car flow
 					telliot_car_move: '%1(으)로 %2초간 이동하기 %3',
-					telliot_car_turn: '%1으로 %2도 회전하기 %3',
+					telliot_car_turn: '%1으로 회전하기 %2',
 					telliot_car_stop: '현재 위치에서 정지하기 %1',
-					telliot_car_wheel_move: '%1 모터를 %2 만큼 %3으로 회전하기 %4',
-					telliot_car_line_move : '라인을 따라 %1(으)로 이동 하기 %2',
+					//telliot_car_wheel_move: '%1 모터를 %2 만큼 %3으로 회전하기 %4',
+					telliot_car_line_move : '라인을 따라 앞으로 이동 하기 %2',
 
 					// Telliot Car sensors
 					telliot_car_sensor_is_detected : '거리 센서가 물체를 %1 했을때 %2',
@@ -62,9 +62,9 @@ class TelliotLite extends TelliotBase {
 					// AI
 //					telliot_car_chatbot : '%1 라고 말하면 %2라고 대답하기 %3',
 //					telliot_car_voice_command : '%1라고 말하면 %2',
-					telliot_car_voice_trigger: '텔리엇에 음성 명령어 전달하기 %1',
+					telliot_car_voice_trigger: '금쪽이 스피커에 음성 명령어 전달하기 %1',
 //					telliot_car_STT: '텔리엇에 전달한 음성을 문자로 전달 받기 %1',
-					telliot_car_TTS: '%1을 텔리엇에서 듣기 %2',
+					telliot_car_TTS: '%1을 금쪽이 스피커에서 듣기 %2',
 
 					// AI prompt
 					telliot_car_add_prompt : '%1 프롬프트 추가하기 %2',
@@ -78,8 +78,8 @@ class TelliotLite extends TelliotBase {
 					telliot_car_move: 'Move %1 during %2sec %3',
 					telliot_car_turn: 'Rotate %2 degree to the %1 %3',
 					telliot_car_stop: 'Stop at current position %1',
-					telliot_car_wheel_move: '%1 모터를 %2 만큼 %3 회전하기 %4',
-					telliot_car_line_move : '라인을 따라 %1하기 %2',
+					//telliot_car_wheel_move: '%1 모터를 %2 만큼 %3 회전하기 %4',
+					telliot_car_line_move : '라인을 따라 앞으로 이동하기 %2',
 
 					// Telliot Car sensors
 					telliot_car_sensor_is_detected : ' 거리 센서가 %1 했을때 %2',
@@ -89,7 +89,7 @@ class TelliotLite extends TelliotBase {
 					// AI
 //					telliot_car_chatbot : '%1 라고 말하면 %2라고 대답하기 %3',
 //					telliot_car_voice_command : '%1라고 말하면 %2',
-					telliot_car_voice_trigger: 'Send voice command to Telliot %1',
+					telliot_car_voice_trigger: 'Send voice command to Speaker %1',
 //					telliot_car_STT: 'Receive speech to text from telliot %1',
 					telliot_car_TTS: 'Speech from %1 %2',
 
@@ -151,7 +151,7 @@ class TelliotLite extends TelliotBase {
 					PREDEFINED_MOTION_VALUE: 1,
                 },
 				class: 'telliot_car_flow',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_predefined_action(sprite, script)
 			},
 
@@ -172,27 +172,27 @@ class TelliotLite extends TelliotBase {
                         bgColor: EntryStatic.colorSet.block.darken.HARDWARE,
                         arrowColor: EntryStatic.colorSet.arrow.default.HARDWARE,
 					},
-					{
-						type: 'Dropdown',
-                        options: [
-                            ['15', '15'],
-                            ['30', '30'],
-                            ['45', '45'],
-                            ['60', '60'],
-                            ['75', '75'],
-                            ['90', '90'],
-                            ['105', '105'],
-                            ['120', '120'],
-                            ['135', '135'],
-                            ['150', '150'],
-                            ['165', '165'],
-                            ['180', '180'],
-                        ],
-                        value: '45',
-                        fontSize: 11,
-                        bgColor: EntryStatic.colorSet.block.darken.HARDWARE,
-                        arrowColor: EntryStatic.colorSet.arrow.default.HARDWARE,
-					},
+					// {
+					// 	type: 'Dropdown',
+                    //     options: [
+                    //         ['15', '15'],
+                    //         ['30', '30'],
+                    //         ['45', '45'],
+                    //         ['60', '60'],
+                    //         ['75', '75'],
+                    //         ['90', '90'],
+                    //         ['105', '105'],
+                    //         ['120', '120'],
+                    //         ['135', '135'],
+                    //         ['150', '150'],
+                    //         ['165', '165'],
+                    //         ['180', '180'],
+                    //     ],
+                    //     value: '45',
+                    //     fontSize: 11,
+                    //     bgColor: EntryStatic.colorSet.block.darken.HARDWARE,
+                    //     arrowColor: EntryStatic.colorSet.arrow.default.HARDWARE,
+					// },
 					{
 						type : 'Indicator',
 						img : 'block_icon/hardware_icon.svg',
@@ -209,7 +209,7 @@ class TelliotLite extends TelliotBase {
 					PREDEFINED_MOTION_VALUE: 1,
                 },
 				class: 'telliot_car_flow',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_predefined_action(sprite, script)
 			},
 
@@ -231,71 +231,71 @@ class TelliotLite extends TelliotBase {
 					type: 'telliot_car_stop',
 				},
 				class: 'telliot_car_flow',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_action_stop(sprite, script)
 			},
 
-			telliot_car_wheel_move : {
-				color: EntryStatic.colorSet.block.default.HARDWARE,
-				outerLine: EntryStatic.colorSet.block.darken.HARDWARE,
-				skeleton: 'basic',
-				statements: [],
-				params: [
-					{
-						type: 'Dropdown',
-                        options: [
-                            ['왼쪽 바퀴', 'left wheel'],
-                            ['오른쪽 바퀴', 'right wheel'],
-                        ],
-                        value: 'left wheel',
-                        fontSize: 11,
-                        bgColor: EntryStatic.colorSet.block.darken.HARDWARE,
-                        arrowColor: EntryStatic.colorSet.arrow.default.HARDWARE,
-					},
-					{
-                        type: 'Block',
-                        accept: 'string',
-						defaultType: 'number',
-                    },
-					{
-						type: 'Dropdown',
-                        options: [
-                            ['시계방향', 'clockwise'],
-                            ['반시계방향', 'counterclockwise'],
-                        ],
-                        value: 'clockwise',
-                        fontSize: 11,
-                        bgColor: EntryStatic.colorSet.block.darken.HARDWARE,
-                        arrowColor: EntryStatic.colorSet.arrow.default.HARDWARE,
-					},
-					{
-						type : 'Indicator',
-						img : 'block_icon/hardware_icon.svg',
-						size : 11,
-					},
-				],
-				events: {},
-				def: {
-					params : [
-						null,
-						{
-							type: 'number',
-							params: ['100'],
-						},
-						null,
-						null,
-					],
-					type: 'telliot_car_wheel_move',
-				},
-				paramsKeyMap: {
-                    MOTOR_PORT_ID: 0,
-					MOTOR_PORT_VALUE: 1,
-					MOTOR_DIRECTION: 2,
-                },
-				class: 'telliot_car_flow',
-				//isNotFor : ['TelliotLite'],
-				func: (sprite, script) => this.req_motor_control(sprite, script)
-			},
+			// telliot_car_wheel_move : {
+			// 	color: EntryStatic.colorSet.block.default.HARDWARE,
+			// 	outerLine: EntryStatic.colorSet.block.darken.HARDWARE,
+			// 	skeleton: 'basic',
+			// 	statements: [],
+			// 	params: [
+			// 		{
+			// 			type: 'Dropdown',
+            //             options: [
+            //                 ['왼쪽 바퀴', 'left wheel'],
+            //                 ['오른쪽 바퀴', 'right wheel'],
+            //             ],
+            //             value: 'left wheel',
+            //             fontSize: 11,
+            //             bgColor: EntryStatic.colorSet.block.darken.HARDWARE,
+            //             arrowColor: EntryStatic.colorSet.arrow.default.HARDWARE,
+			// 		},
+			// 		{
+            //             type: 'Block',
+            //             accept: 'string',
+			// 			defaultType: 'number',
+            //         },
+			// 		{
+			// 			type: 'Dropdown',
+            //             options: [
+            //                 ['시계방향', 'clockwise'],
+            //                 ['반시계방향', 'counterclockwise'],
+            //             ],
+            //             value: 'clockwise',
+            //             fontSize: 11,
+            //             bgColor: EntryStatic.colorSet.block.darken.HARDWARE,
+            //             arrowColor: EntryStatic.colorSet.arrow.default.HARDWARE,
+			// 		},
+			// 		{
+			// 			type : 'Indicator',
+			// 			img : 'block_icon/hardware_icon.svg',
+			// 			size : 11,
+			// 		},
+			// 	],
+			// 	events: {},
+			// 	def: {
+			// 		params : [
+			// 			null,
+			// 			{
+			// 				type: 'number',
+			// 				params: ['100'],
+			// 			},
+			// 			null,
+			// 			null,
+			// 		],
+			// 		type: 'telliot_car_wheel_move',
+			// 	},
+			// 	paramsKeyMap: {
+            //         MOTOR_PORT_ID: 0,
+			// 		MOTOR_PORT_VALUE: 1,
+			// 		MOTOR_DIRECTION: 2,
+            //     },
+			// 	class: 'telliot_car_flow',
+			// 	//isNotFor : ['TelliotLite'],
+			// 	func: (sprite, script) => this.req_motor_control(sprite, script)
+			// },
 
 			telliot_car_line_move : {
 				color: EntryStatic.colorSet.block.default.HARDWARE,
@@ -307,7 +307,7 @@ class TelliotLite extends TelliotBase {
 						type: 'Dropdown',
                         options: [
                             ['앞으로', 'forward'],
-                            ['뒤로', 'backward'],
+                           // ['뒤로', 'backward'],
                         ],
                         value: 'forward',
                         fontSize: 11,
@@ -329,7 +329,7 @@ class TelliotLite extends TelliotBase {
                     LINE_TRACE_DIR: 0,
                 },
 				class: 'telliot_car_flow',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_line_trace(sprite, script)
 			},
 
@@ -366,7 +366,7 @@ class TelliotLite extends TelliotBase {
                     OBJECT_DETECT: 0,
                 },
 				class: 'telliot_car_sensors',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_detect_object(sprite, script),
 			},
 
@@ -406,7 +406,7 @@ class TelliotLite extends TelliotBase {
                     TAG_DETECT: 0,
                 },
 				class: 'telliot_car_sensors',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_rfid_tag_detect(sprite, script),
 			},
 
@@ -477,7 +477,7 @@ class TelliotLite extends TelliotBase {
 					AI_CHATBOT_RESP: 1,
               },
 				class: 'telliot_ai',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_add_chatbot(sprite, script)
 			},
 
@@ -513,7 +513,7 @@ class TelliotLite extends TelliotBase {
                     CAR_CHATBOT_CMD: 0,
                 },
 				class: 'telliot_ai',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func(sprite, script) {
 					return script.callReturn();
 				},
@@ -538,7 +538,7 @@ class TelliotLite extends TelliotBase {
 					type: 'telliot_car_voice_trigger',
 				},
 				class: 'telliot_ai',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_voice_trigger(sprite, script)
 			},
 
@@ -561,7 +561,7 @@ class TelliotLite extends TelliotBase {
 					type: 'telliot_car_STT',
 				},
 				class: 'telliot_ai',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_speech_to_text(sprite, script)
 			},
 
@@ -597,7 +597,7 @@ class TelliotLite extends TelliotBase {
                     TEXT_TO_SPEECH: 0,
                 },
 				class: 'telliot_ai',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_text_to_speech(sprite, script)
 			},
 
@@ -633,7 +633,7 @@ class TelliotLite extends TelliotBase {
                     AI_PROMPT: 0,
                 },
 				class: 'telliot_ai',
-				//isNotFor : ['TelliotLite'],
+				isNotFor : ['GoldenKidsAICar'],
 				func: (sprite, script) => this.req_add_prompt(sprite, script)
 			},
 
