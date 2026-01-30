3bf057abc26133e0f0429e722f589db1194361e3
kim jeong yong <usbsync@nts-corp.com>
2026-01-30 15:42:59 +0900

    Refactor formatting in block_func.js for improved readability. Adjusted line breaks for better clarity in the assignment of the first block.

--- src/playground/blocks/block_func.js
+++ src/playground/blocks/block_func.js
@@ -467,8 +467,9 @@ module.exports = {
 
                     this.funcExecutor.result = this.funcExecutor.scope;
 
-                    const firstBlock = this.funcExecutor?.scope?.block?.statements[0]?.getFirstBlock();
-                    if(firstBlock) {
+                    const firstBlock =
+                        this.funcExecutor?.scope?.block?.statements[0]?.getFirstBlock();
+                    if (firstBlock) {
                         this.funcExecutor.scope = new Entry.Scope(firstBlock, this.funcExecutor);
                     }
                     const { promises } = this.funcExecutor.execute();
